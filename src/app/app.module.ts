85cf068313bf4764296c05152e9899655c15ef2c
luotao <luotaoyeah@gmail.com>
2019-03-18 21:56:28 +0800

    doc: tutorial: heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HerosComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
