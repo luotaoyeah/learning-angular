879e9eb387331ece6c893a2cb9ff9c670ea0fbd2
luotao <luotaoyeah@gmail.com>
2019-03-19 11:40:14 +0800

    doc: tutorial: hero detail component: @Input()

--- src/app/tutorial/hero-detail/hero-detail.component.ts
+++ src/app/tutorial/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -7,7 +7,11 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-  hero: Hero | null = null;
+  /*
+   * @Input 用来声明一个 input property，类似于 vue 里面的 props property
+   */
+  @Input('my-hero') hero: Hero | null = null;
+
   constructor() {}
 
   ngOnInit() {}
