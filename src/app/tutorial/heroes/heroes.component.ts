83f9c6d7ffbf16fb736dce982e10c3401e2c9930
luotao <luotaoyeah@gmail.com>
2019-03-19 11:12:14 +0800

    doc: tutorial: heroes component: (click)

--- src/app/tutorial/heroes/heroes.component.ts
+++ src/app/tutorial/heroes/heroes.component.ts
@@ -8,9 +8,20 @@ import { HEROES } from '../heroes-mock';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
+  /** 当前选中的英雄 */
+  selectedHero: Hero | null = null;
+  /** 英雄列表 */
   heroes: Array<Hero> = HEROES;
 
   constructor() {}
 
   ngOnInit() {}
+
+  /**
+   * 选择某个英雄
+   * @param hero 选择的英雄
+   */
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
