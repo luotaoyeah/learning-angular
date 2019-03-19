e6ce46a0e7ea4d83e3cd28fb29a3cd25f742ad69
luotao <luotaoyeah@gmail.com>
2019-03-19 09:49:22 +0800

    doc: tutorial: heroes component: ngModel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,7 @@ import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
