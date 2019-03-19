6a3e8d95ab85b35e58250295fcb961acd286a6f8
luotao <luotaoyeah@gmail.com>
2019-03-19 11:30:16 +0800

    doc: tutorial: hero detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './tutorial/heroes/heroes.component';
+import { HeroDetailComponent } from './tutorial/hero-detail/hero-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
