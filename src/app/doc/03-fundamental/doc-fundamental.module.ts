ca59bf642bbd25d88357915e7b002d5013df42ce
luotao <luotaoyeah@gmail.com>
2019-03-20 21:55:43 +0800

    doc: fundamental: architecture: intro to component: template

--- src/app/doc/03-fundamental/doc-fundamental.module.ts
+++ src/app/doc/03-fundamental/doc-fundamental.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IntroIntoComponentAComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-a.component';
 import { IntroIntoComponentBComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-b.component';
+import { IntroIntoComponentCComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-c.component';
 
 /**
  * DOC: FUNDAMENTAL
  */
 @NgModule({
-  declarations: [IntroIntoComponentAComponent, IntroIntoComponentBComponent],
+  declarations: [IntroIntoComponentAComponent, IntroIntoComponentBComponent, IntroIntoComponentCComponent],
   imports: [],
-  exports: [IntroIntoComponentAComponent, IntroIntoComponentBComponent]
+  exports: [IntroIntoComponentAComponent, IntroIntoComponentBComponent, IntroIntoComponentCComponent]
 })
 export class DocFundamentalModule {}
