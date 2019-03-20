05d7863562afe479520acc1ec0b5f0ddd592f197
luotao <luotaoyeah@gmail.com>
2019-03-20 21:19:39 +0800

    doc: fundamental: architecture: intro to components: delete spec files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 
+/*
+ * 使用 @Component 装饰器将一个类标识为一个 component
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: []
 })
 export class AppComponent {
   title = 'TOUR OF HEROES';
