96db29d26afd4e1f3e50ee2bc236a4683e0c0b27
luotao <luotaoyeah@gmail.com>
2019-03-21 11:56:32 +0800

    adopt primeng

--- src/app/doc/03-fundamental/doc-fundamental.module.ts
+++ src/app/doc/03-fundamental/doc-fundamental.module.ts
@@ -8,6 +8,8 @@ import { FormsModule } from '@angular/forms';
 import { DashPipe } from './03-01-architecture/03-01-03-intro-to-component/dash.pipe';
 import { RadiusDirective } from './03-01-architecture/03-01-03-intro-to-component/radius.directive';
 import { DiAComponent } from './03-01-architecture/03-01-04-di/di-a.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/primeng';
 
 /**
  * DOC: FUNDAMENTAL
@@ -22,7 +24,7 @@ import { DiAComponent } from './03-01-architecture/03-01-04-di/di-a.component';
     RadiusDirective,
     DiAComponent
   ],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ...[ButtonModule, InputTextModule]],
   exports: [
     IntroIntoComponentAComponent,
     IntroIntoComponentBComponent,
