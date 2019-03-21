3c04e95d18ba6a259be7dfd20027e4ed6ab55de1
luotao <luotaoyeah@gmail.com>
2019-03-21 09:33:38 +0800

    doc: fundamental: architecture: intro to component: template syntax

--- src/app/doc/03-fundamental/doc-fundamental.module.ts
+++ src/app/doc/03-fundamental/doc-fundamental.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { IntroIntoComponentAComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-a.component';
 import { IntroIntoComponentBComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-b.component';
 import { IntroIntoComponentCComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-c.component';
+import { IntroIntoComponentDComponent } from './03-01-architecture/03-01-03-intro-to-component/intro-into-component-d.component';
+import { CommonModule } from '@angular/common';
 
 /**
  * DOC: FUNDAMENTAL
  */
 @NgModule({
-  declarations: [IntroIntoComponentAComponent, IntroIntoComponentBComponent, IntroIntoComponentCComponent],
-  imports: [],
-  exports: [IntroIntoComponentAComponent, IntroIntoComponentBComponent, IntroIntoComponentCComponent]
+  declarations: [IntroIntoComponentAComponent, IntroIntoComponentBComponent, IntroIntoComponentCComponent, IntroIntoComponentDComponent],
+  imports: [CommonModule],
+  exports: [IntroIntoComponentAComponent, IntroIntoComponentBComponent, IntroIntoComponentCComponent, IntroIntoComponentDComponent]
 })
 export class DocFundamentalModule {}
