f5fd48174da35bd961dd971a943da33a66a9f032
luotao <luotaoyeah@gmail.com>
2019-03-22 09:09:00 +0800

    doc: fundamental: routing: ActivatedRoute, Router, RouterState

--- src/app/doc/03-fundamental/03-09-routing-navigation/routing-navigation.component.ts
+++ src/app/doc/03-fundamental/03-09-routing-navigation/routing-navigation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-routing-navigation',
@@ -7,12 +7,14 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./routing-navigation.component.scss']
 })
 export class RoutingNavigationComponent implements OnInit {
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
   ngOnInit() {
     /*
      * 通过 ActivatedRoute.data 属性，可以获取到路由配置中的 data 对象
      */
-    console.log(this.activatedRoute.snapshot.data);
+    console.assert(this.activatedRoute.snapshot.data.someData === 'FOO');
+
+    console.assert(this.activatedRoute.root === this.router.routerState.root);
   }
 }
