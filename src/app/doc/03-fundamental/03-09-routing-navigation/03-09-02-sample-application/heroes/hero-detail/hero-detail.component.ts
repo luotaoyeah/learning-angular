d382f5fbe9a3eb7ab6cb357d2369ed6e170fdbee
luotao <luotaoyeah@gmail.com>
2019-03-23 16:45:01 +0800

    doc: fundamental: routing: sample application: ActivatedRoute.snapshot

--- src/app/doc/03-fundamental/03-09-routing-navigation/03-09-02-sample-application/heroes/hero-detail/hero-detail.component.ts
+++ src/app/doc/03-fundamental/03-09-routing-navigation/03-09-02-sample-application/heroes/hero-detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../model/hero';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { HeroService } from '../service/hero.service';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
@@ -14,11 +16,41 @@ export class HeroDetailComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private heroService: HeroService) {}
 
   ngOnInit() {
+    /*
+     * ActivatedRoute 中的属性默认都是 observable 对象，
+     * 相对应的，ActivatedRoute.snapshot 对象中的属性，都是非 observable 对象，
+     * 当某个组件会被重用时，比如根据不同的 ID 访问不同英雄的详情页，
+     * 此时该详情页是被重用的，只是每次的 ID 参数不一样，
+     * 在这种情况下，需要使用 observable 版本的属性，因为它们可以检测到 ID 的变化，
+     * 而 snapshot 版本中的属性，不能检测到 ID 的变化，只能获取到组件初次加载时的值
+     */
+    // this.getHero();
+    this.getHero$();
+  }
+
+  getHero(): void {
     const id: number = Number(this.activatedRoute.snapshot.paramMap.get('id'));
     if (!Number.isNaN(id)) {
       this.heroService.getHero(id).subscribe((hero: Hero) => {
         this.hero = hero;
       });
     }
+  }
+
+  getHero$(): void {
+    this.activatedRoute.paramMap
+      .pipe(
+        switchMap((paramMap: ParamMap) => {
+          const id: number = Number(paramMap.get('id'));
+          if (!Number.isNaN(id)) {
+            return this.heroService.getHero(id);
+          }
+
+          return of(null);
+        })
+      )
+      .subscribe((hero: Hero | null) => {
+        this.hero = hero;
+      });
   }
 }
