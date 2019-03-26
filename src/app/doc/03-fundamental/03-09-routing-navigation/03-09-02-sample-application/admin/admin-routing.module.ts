ac8d092e739cd924b62278238193c4b2ec41a01e
luotao <luotaoyeah@gmail.com>
2019-03-26 14:59:49 +0800

    doc: fundamental: routing: sample application: canActivateChild

--- src/app/doc/03-fundamental/03-09-routing-navigation/03-09-02-sample-application/admin/admin-routing.module.ts
+++ src/app/doc/03-fundamental/03-09-routing-navigation/03-09-02-sample-application/admin/admin-routing.module.ts
@@ -19,6 +19,7 @@ const routes: Routes = [
         children: [
           {
             path: '',
+            canActivateChild: [AuthGuard],
             children: [
               {
                 path: '',
--- src/app/doc/03-fundamental/03-09-routing-navigation/03-09-02-sample-application/auth/service/auth.guard.ts
+++ src/app/doc/03-fundamental/03-09-routing-navigation/03-09-02-sample-application/auth/service/auth.guard.ts
@@ -1,20 +1,38 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private router: Router, private authService: AuthService) {}
 
+  /**
+   * canActivate() 用来守护某个路由
+   * @param next 要跳转的路由
+   * @param state 当前路由状态
+   */
   canActivate(
     // @ts-ignore
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.checkLogin(state.url);
+  }
+
+  /**
+   * canActivateChild() 用来守护下级路由
+   * @param childRoute 下级路由
+   * @param state 当前路由状态
+   */
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    // @ts-ignore
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return !!childRoute.routeConfig && childRoute.routeConfig.path !== 'heroes';
   }
 
   /**
