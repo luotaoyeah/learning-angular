a8e19343330ddccd93a430438639f1e3f3378839
luotao <luotaoyeah@gmail.com>
2019-03-29 17:43:57 +0800

    doc: fundamental: component & template: the non-null assertion operator

---
+++ src/app/doc/03-fundamental/03-02-component-template/03-02-02-template-syntax/03-02-02-13-template-expression-operators/03-02-02-13-03-the-non-null-assertion-operator/doc-03-02-02-13-03.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-03-02-02-13-03',
+  templateUrl: './doc-03-02-02-13-03.component.html'
+})
+export class Doc0302021303Component implements OnInit {
+  boy: { id: number; name: string } | null = null;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
