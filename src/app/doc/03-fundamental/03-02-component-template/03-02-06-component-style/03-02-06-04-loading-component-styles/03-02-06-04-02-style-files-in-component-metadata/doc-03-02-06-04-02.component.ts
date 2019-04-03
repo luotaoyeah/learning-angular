bdfc5fa4a5d918010be6326a4b1ef9b2dc4ad98f
luotao <luotaoyeah@gmail.com>
2019-04-03 09:31:35 +0800

    doc: fundamental: component style: style files in component metadata

---
+++ src/app/doc/03-fundamental/03-02-component-template/03-02-06-component-style/03-02-06-04-loading-component-styles/03-02-06-04-02-style-files-in-component-metadata/doc-03-02-06-04-02.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-03-02-06-04-02',
+  templateUrl: './doc-03-02-06-04-02.component.html',
+  styleUrls: ['./doc-03-02-06-04-02.component.css', './doc-03-02-06-04-02.component.scss'],
+  styles: ['p{font-weight: bold}']
+})
+export class Doc0302060402Component implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
