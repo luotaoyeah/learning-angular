123c98bc2a26ff24687ad6ea166ec2de3e727b04
luotao <luotaoyeah@gmail.com>
2019-04-13 23:48:31 +0800

    feat: add commit message hook

---
+++ scripts/commit-msg.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+const messagePath = process.env.HUSKY_GIT_PARAMS;
+const message = require('fs')
+  .readFileSync(messagePath, 'utf-8')
+  .trim();
+
+const commitRegExp = /^(revert: )?(feat|fix|polish|docs|style|refactor|perf|test|workflow|ci|chore|types|build)(\(.+\))?: .{1,50}/;
+
+if (!commitRegExp.test(message)) {
+  console.log();
+  console.error(
+    `  ${chalk.bgRed.white('ERROR')} ${chalk.red(
+      `INVALID COMMIT MESSAGE FORMAT.`,
+    )}\n\n` +
+      chalk.red(`  VALID EXAMPLES:\n\n`) +
+      `    ${chalk.green(`feat(compiler): add 'comments' option`)}\n` +
+      `    ${chalk.green(`fix(model): handle events on blur (close #28)`)}\n\n`,
+  );
+  process.exit(1);
+}
