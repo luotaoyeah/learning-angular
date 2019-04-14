2126ed286fc77a417481db7c357bbef401eae097
luotao <luotaoyeah@gmail.com>
2019-04-14 15:00:25 +0800

    feat: doc: fundamental: form: template-driven form: ngModel

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-03-template-driven-form/03-03-03-01-introduction-to-template-driven-forms/doc-03-03-03-01.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-03-template-driven-form/03-03-03-01-introduction-to-template-driven-forms/doc-03-03-03-01.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../model/Hero';
 
 @Component({
   selector: 'app-doc-03-03-03-01',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doc-03-03-03-01.component.scss'],
 })
 export class Doc03030301Component implements OnInit {
+  powers: Array<string> = [
+    'Really Smart',
+    'Super Flexible',
+    'Super Hot',
+    'Weather Changer',
+  ];
+
+  model: Hero = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted: boolean = false;
+
+  onSubmit() {
+    this.submitted = true;
+  }
+
   constructor() {}
 
   ngOnInit() {}
