1cf385bd10e4c901b057d08739fdf96ee65b7dc0
luotao <luotaoyeah@gmail.com>
2019-04-18 20:24:12 +0800

    feat: doc: fundamental: NgModule: introduction

--- src/app/routes/doc/03-fundamental/03-05-bootstrapping/doc-03-05.component.ts
+++ src/app/routes/doc/03-fundamental/03-05-bootstrapping/doc-03-05.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-/*
- * TODO https://angular.io/guide/bootstrapping
- */
 @Component({
   selector: 'app-doc-03-05',
   templateUrl: './doc-03-05.component.html',
