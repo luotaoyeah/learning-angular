22cfc255e979bc1510dcdaf0ccfdc49544e5a7e4
luotao <luotaoyeah@gmail.com>
2019-04-18 20:41:33 +0800

    feat: doc: fundamental: ngmodule: frequently used modules(BrowserModule, CommonModule, FormsModule, ReactiveFormsModule, RouterModule, HttpClientModule)

--- src/app/routes/doc/03-fundamental/03-06-ngmodules/03-06-03-frequent-ngmodules/doc-03-06-03.component.ts
+++ src/app/routes/doc/03-fundamental/03-06-ngmodules/03-06-03-frequent-ngmodules/doc-03-06-03.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-/*
- * TODO https://angular.io/guide/frequent-ngmodules
- */
 @Component({
   selector: 'app-doc-03-06-03',
   templateUrl: './doc-03-06-03.component.html',
