fc8bb3f0163aad94a04c8ef12d559c5144ed042f
luotao <luotaoyeah@gmail.com>
2019-04-22 16:48:39 +0800

    feat: doc: fundamental: di: injecting services

--- src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-01-angular-dependency-injection/doc-03-07-01.component.ts
+++ src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-01-angular-dependency-injection/doc-03-07-01.component.ts
@@ -8,6 +8,11 @@ import { Doc030701Service } from './service/doc-03-07-01.service';
 export class Doc030701Component implements OnInit {
   public color!: string;
 
+  /*
+   * 如何注入一个 service？
+   * 对于 component 来说，在 component 的 constructor 的 parameters 中声明需要注入的 service 类型就可以了，
+   * 如下，声明了一个 Doc030701Service 类型的参数，在创建该组件的实例时，angular 的 injector 会自动注入这个 service
+   */
   constructor(private doc030701Service: Doc030701Service) {}
 
   ngOnInit() {
