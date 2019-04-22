48e54c3f6792195ff8c7b4daafeea3b9660414a9
luotao <luotaoyeah@gmail.com>
2019-04-22 09:35:55 +0800

    feat: doc: fundamental: ngmodule: sharing modules

--- src/app/routes/doc/03-fundamental/03-06-ngmodules/03-06-10-sharing-ngmodules/doc-03-06-10.component.ts
+++ src/app/routes/doc/03-fundamental/03-06-ngmodules/03-06-10-sharing-ngmodules/doc-03-06-10.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-/*
- * TODO https://angular.io/guide/sharing-ngmodules
- */
 @Component({
   selector: 'app-doc-03-06-10',
   templateUrl: './doc-03-06-10.component.html',
