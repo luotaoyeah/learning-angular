d2e4e15597721ea28d14c14962d54d1eadd44965
luotao <luotaoyeah@gmail.com>
2019-04-22 17:34:53 +0800

    feat: doc: fundamental: di: @Optional()

--- src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-01-angular-dependency-injection/doc-03-07-01.component.ts
+++ src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-01-angular-dependency-injection/doc-03-07-01.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Optional } from '@angular/core';
 import { Doc030701Service } from './service/doc-03-07-01.service';
+import { Doc03070102Service } from './service/doc-03-07-01-02.service';
 
 @Component({
   selector: 'app-doc-03-07-01',
@@ -13,7 +14,14 @@ export class Doc030701Component implements OnInit {
    * 对于 component 来说，在 component 的 constructor 的 parameters 中声明需要注入的 service 类型就可以了，
    * 如下，声明了一个 Doc030701Service 类型的参数，在创建该组件的实例时，angular 的 injector 会自动注入这个 service
    */
-  constructor(private doc030701Service: Doc030701Service) {}
+
+  constructor(
+    private doc030701Service: Doc030701Service,
+    // @ts-ignore
+    @Optional() private doc03070102Service: Doc03070102Service,
+  ) {
+    console.assert(doc03070102Service === null);
+  }
 
   ngOnInit() {
     this.color = this.doc030701Service.getColor();
