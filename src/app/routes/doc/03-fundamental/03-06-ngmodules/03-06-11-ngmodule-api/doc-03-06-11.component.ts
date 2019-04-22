9c8052483d6c8e3d651e5e2866d4eafa17f016f1
luotao <luotaoyeah@gmail.com>
2019-04-22 10:37:32 +0800

    feat: doc: fundamental: ngmodule: NgModule API

--- src/app/routes/doc/03-fundamental/03-06-ngmodules/03-06-11-ngmodule-api/doc-03-06-11.component.ts
+++ src/app/routes/doc/03-fundamental/03-06-ngmodules/03-06-11-ngmodule-api/doc-03-06-11.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-/*
- * TODO https://angular.io/guide/ngmodule-api
- */
 @Component({
   selector: 'app-doc-03-06-11',
   templateUrl: './doc-03-06-11.component.html',
