9811de474058bcc80c33f17e070dd8e30361dbef
luotao <luotaoyeah@gmail.com>
2019-04-23 17:12:15 +0800

    feat: doc: fundamental: di: dependency providers

--- src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-03-di-in-action/doc-03-07-03.component.ts
+++ src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-03-di-in-action/doc-03-07-03.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-/*
- * TODO https://angular.io/guide/dependency-injection-providers
- */
 @Component({
   selector: 'app-doc-03-07-03',
   templateUrl: './doc-03-07-03.component.html',
