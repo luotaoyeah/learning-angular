4adaa6aaa7aac464983025d4b81558658915f329
luotao <luotaoyeah@gmail.com>
2019-04-25 10:03:09 +0800

    feat: doc: fundamental: di: useExisting

--- src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-04-di-in-action/03-07-04-05-define-dependencies-with-providers/doc-03-07-04-05.component.ts
+++ src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-04-di-in-action/03-07-04-05-define-dependencies-with-providers/doc-03-07-04-05.component.ts
@@ -3,6 +3,7 @@ import {
   Foo,
   SOME_TOKEN_01,
   SOME_TOKEN_02,
+  SOME_TOKEN_03,
 } from './service/doc-03-07-04-05.service';
 
 @Component({
@@ -17,6 +18,15 @@ import {
       provide: SOME_TOKEN_02,
       useClass: Foo,
     },
+    /*
+     * useExisting 的作用是，让多个不同的 token 共用一个 provider，
+     * 或者说，给某个 token 起了一个别名（alias），使用这个别名获取到的是同样的实例，
+     * useExisting 后面的值是一个 token
+     */
+    {
+      provide: SOME_TOKEN_03,
+      useExisting: SOME_TOKEN_02,
+    },
   ],
 })
 export class Doc03070405Component implements OnInit {
@@ -25,10 +35,13 @@ export class Doc03070405Component implements OnInit {
     private token01: string,
     @Inject(SOME_TOKEN_02)
     private token02: Foo,
+    @Inject(SOME_TOKEN_03)
+    private token03: Foo,
   ) {}
 
   ngOnInit() {
     console.assert(this.token01 === 'FOO');
     console.assert(this.token02.foo === 'FOO');
+    console.assert(this.token02 === this.token03);
   }
 }
--- src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-04-di-in-action/03-07-04-05-define-dependencies-with-providers/service/doc-03-07-04-05.service.ts
+++ src/app/routes/doc/03-fundamental/03-07-dependency-injection/03-07-04-di-in-action/03-07-04-05-define-dependencies-with-providers/service/doc-03-07-04-05.service.ts
@@ -6,5 +6,6 @@ class Foo {
 
 const SOME_TOKEN_01 = new InjectionToken<string>('some.token.01');
 const SOME_TOKEN_02 = new InjectionToken<Foo>('some.token.02');
+const SOME_TOKEN_03 = new InjectionToken<Foo>('some.token.03');
 
-export { SOME_TOKEN_01, SOME_TOKEN_02, Foo };
+export { SOME_TOKEN_01, SOME_TOKEN_02, SOME_TOKEN_03, Foo };
