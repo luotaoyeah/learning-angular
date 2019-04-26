905e0d5d78021cfba187a1f3c33d7046b900c678
luotao <luotaoyeah@gmail.com>
2019-04-26 10:51:23 +0800

    feat(doc.fundamental.http): retry()

--- src/app/routes/doc/03-fundamental/03-08-httpclient/03-08-02-error-handling/service/doc-03-08-02.service.ts
+++ src/app/routes/doc/03-fundamental/03-08-httpclient/03-08-02-error-handling/service/doc-03-08-02.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { IConfig } from '../model/IConfig';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -12,6 +13,19 @@ export class Doc030802Service {
   constructor(private httpClient: HttpClient) {}
 
   getConfig(): Observable<IConfig> {
-    return this.httpClient.get<IConfig>(this.configUrl);
+    return this.httpClient.get<IConfig>(this.configUrl).pipe(
+      /*
+       * 可以使用 retry() 操作符，对请求进行重试，
+       * 即如果请求失败，则自动重新发起新的请求，可以指定重试的次数
+       */
+      retry(1),
+      /*
+       * 可以使用 catchError() 操作符，对错误进行处理，
+       * HttpClient 如果请求失败，返回的是一个 HttpErrorResponse 对象
+       */
+      catchError((e: HttpErrorResponse) => {
+        return throwError(e);
+      }),
+    );
   }
 }
