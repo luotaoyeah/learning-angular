b54645349b5dafb12f57f8c4ba02dbf1ec91df4f
luotao <luotaoyeah@gmail.com>
2019-04-27 13:51:00 +0800

    feat(doc.fundamental.http): HttpTestingController.expectOne()

--- src/app/routes/doc/03-fundamental/03-08-httpclient/03-08-06-testing-http-requests/doc-03-08-06.component.spec.ts
+++ src/app/routes/doc/03-fundamental/03-08-httpclient/03-08-06-testing-http-requests/doc-03-08-06.component.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpRequest } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
@@ -14,6 +14,9 @@ describe('Doc030806Component', () => {
   let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
 
+  /*
+   * HttpTestingController 用来在测试时，对请求进行拦截和模拟
+   */
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -30,12 +33,47 @@ describe('Doc030806Component', () => {
       expect(result).toBe(mockResult);
     });
 
+    /*
+     * HttpTestingController.expectOne() 方法，表示期望匹配一个请求，
+     * 如果没有匹配的请求，或者匹配的请求有多个，则会失败，
+     * 如果成功，则返回匹配到的请求
+     */
     const testRequest: TestRequest = httpTestingController.expectOne('/foo');
 
     expect(testRequest.request.method).toEqual('GET');
 
+    /*
+     * TestRequest.flush() 方法，用来模拟请求响应，返回数据
+     */
     testRequest.flush(mockResult);
 
     httpTestingController.verify();
+  });
+
+  it('should match request by header', () => {
+    const mockResult: IResult = { data: 'FOO' };
+
+    httpClient
+      .get<IResult>('/foo', {
+        headers: new HttpHeaders({
+          H01: 'H01',
+        }),
+      })
+      .subscribe((result: IResult) => {
+        expect(result).toBe(mockResult);
+      });
+
+    /*
+     * HttpTestingController.expectOne() 方法，
+     * 除了可以使用请求地址进行匹配之外，还可以使用自定义的逻辑进行匹配
+     */
+    const testRequest = httpTestingController.expectOne(
+      // tslint:disable-next-line:no-any
+      (httpRequest: HttpRequest<any>) => {
+        return httpRequest.headers.has('H01');
+      },
+    );
+
+    testRequest.flush(mockResult);
   });
 });
