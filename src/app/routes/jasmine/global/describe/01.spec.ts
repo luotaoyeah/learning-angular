20d6dc9f96c0c83f3324c1d50647c144fa5c636f
luotao <luotaoyeah@gmail.com>
2019-04-28 17:04:40 +0800

    feat(jasmine.global): delete fdescribe()

--- src/app/routes/jasmine/global/describe/01.spec.ts
+++ src/app/routes/jasmine/global/describe/01.spec.ts
@@ -1,17 +1,13 @@
 /*
  * describe() 方法，用来创建一组测试用例，俗称一个套件（suite）
  */
-describe('01', () => {
-  it('should working', () => {
-    expect(0).toBeFalsy();
-  });
+describe('jasmine.global.describe.01', () => {
+  it('01', () => {});
 
   /*
    * describe() 可以嵌套，用来组成一个树形的套件树
    */
   describe('01', () => {
-    it('should working', () => {
-      expect(9).toBeTruthy();
-    });
+    it('01', () => {});
   });
 });
