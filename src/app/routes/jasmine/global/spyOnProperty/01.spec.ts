351b36f7fdbb93a657887a5206853996d0bbcfae
luotao <luotaoyeah@gmail.com>
2019-04-28 21:03:31 +0800

    feat(jasmine.global): spyOnProperty()

---
+++ src/app/routes/jasmine/global/spyOnProperty/01.spec.ts
@@ -0,0 +1,35 @@
+/*
+ * spyOnProperty() 方法，
+ * 对某个 object 的某个 access property 进行监视，返回被监视的 property
+ */
+
+import Spy = jasmine.Spy;
+
+describe('jasmine.global.spyOnProperty.01', () => {
+  it('01', () => {
+    const obj: { name?: string } = {};
+    Object.defineProperty(obj, 'name', {
+      get(): string {
+        return 'FOO';
+      },
+    });
+    const spy: Spy = spyOnProperty(obj, 'name', 'get');
+
+    spy.and.returnValue('BAR');
+
+    expect(obj.name).toEqual('BAR');
+  });
+
+  it('02', () => {
+    const obj: { name: string } = {
+      get name() {
+        return 'FOO';
+      },
+    };
+
+    const spy = spyOnProperty(obj, 'name');
+    spy.and.returnValue('BAZ');
+
+    expect(obj.name).toEqual('BAZ');
+  });
+});
