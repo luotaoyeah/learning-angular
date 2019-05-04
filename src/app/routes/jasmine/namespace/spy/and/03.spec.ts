e5a1c5f103267b4b664425845d8b86819b93f0b2
luotao <luotaoyeah@gmail.com>
2019-05-04 09:41:53 +0800

    feat(jasmine.namespace.spy.and): jasmine.SpyAnd.identity()

---
+++ src/app/routes/jasmine/namespace/spy/and/03.spec.ts
@@ -0,0 +1,12 @@
+/*
+ * jasmine.SpyAnd.identity() 方法，
+ * 返回 spy 对象的标识信息
+ */
+
+describe('jasmine.namespace.spy.and.identity.01', () => {
+  it('01', () => {
+    const spyFoo = jasmine.createSpy('foobar');
+    // @ts-ignore
+    expect(spyFoo.and.identity()).toEqual('foobar');
+  });
+});
