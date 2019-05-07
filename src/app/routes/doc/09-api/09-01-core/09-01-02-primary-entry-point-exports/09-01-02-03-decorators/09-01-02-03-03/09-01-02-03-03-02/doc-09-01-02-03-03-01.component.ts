78c2ad26da57694102550bdd07b5a3965adbfaaf
luotao <luotaoyeah@gmail.com>
2019-05-07 19:15:20 +0800

    feat(doc.api.core): <ng-content>

---
+++ src/app/routes/doc/09-api/09-01-core/09-01-02-primary-entry-point-exports/09-01-02-03-decorators/09-01-02-03-03/09-01-02-03-03-02/doc-09-01-02-03-03-01.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doc-09-01-02-03-03-01',
+  templateUrl: './doc-09-01-02-03-03-01.component.html',
+})
+export class Doc090102030301Component implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/routes/doc/09-api/09-01-core/doc-09-01.module.ts
+++ src/app/routes/doc/09-api/09-01-core/doc-09-01.module.ts
@@ -6,14 +6,14 @@ import { Doc0901RoutingModule } from './doc-09-01-routing.module';
 import { Doc090102Component } from './09-01-02-primary-entry-point-exports/doc-09-01-02.component';
 import { Doc090101Component } from './09-01-01-entry-points/doc-09-01-01.component';
 import { Doc09010203Component } from './09-01-02-primary-entry-point-exports/09-01-02-03-decorators/doc-09-01-02-03.component';
-import { Doc0901020303Component } from './09-01-02-primary-entry-point-exports/09-01-02-03-decorators/09-01-02-03-03/doc-09-01-02-03-03.component';
+import { Doc090102030301Component } from './09-01-02-primary-entry-point-exports/09-01-02-03-decorators/09-01-02-03-03/09-01-02-03-03-01/doc-09-01-02-03-03-01.component';
 
 @NgModule({
   declarations: [
     Doc090101Component,
     Doc090102Component,
     Doc09010203Component,
-    Doc0901020303Component,
+    Doc090102030301Component,
   ],
   imports: [CommonModule, FormsModule, SharedModule, Doc0901RoutingModule],
 })
