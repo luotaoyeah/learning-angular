feac69e3b854f788327b9a90231de5e34291e913
luotao <luotaoyeah@gmail.com>
2019-05-11 18:45:54 +0800

    feat(doc.api.common): CurrencyPipe

---
+++ src/app/routes/doc/09-api/09-02-common/09-02-02-primary-entry-point-exports/09-02-02-06-pipes/09-02-02-06-02-currency-pipe/doc-09-02-02-06-02.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+/*
+ * https://angular.io/api/common/CurrencyPipe
+ */
+@Component({
+  selector: 'app-doc-09-02-02-06-02',
+  templateUrl: './doc-09-02-02-06-02.component.html',
+})
+export class Doc0902020602Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
--- src/app/routes/doc/09-api/09-02-common/doc-09-02.module.ts
+++ src/app/routes/doc/09-api/09-02-common/doc-09-02.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@shared';
+import { Doc0902RoutingModule } from './doc-09-02-routing.module';
 import { Doc090202Component } from './09-02-02-primary-entry-point-exports/doc-09-02-02.component';
 import { Doc09020206Component } from './09-02-02-primary-entry-point-exports/09-02-02-06-pipes/doc-09-02-02-06.component';
 import { Doc0902020601Component } from './09-02-02-primary-entry-point-exports/09-02-02-06-pipes/09-02-02-06-01-async-pipe/doc-09-02-02-06-01.component';
-import { SharedModule } from '@shared';
-import { Doc0902RoutingModule } from './doc-09-02-routing.module';
+import { Doc0902020602Component } from './09-02-02-primary-entry-point-exports/09-02-02-06-pipes/09-02-02-06-02-currency-pipe/doc-09-02-02-06-02.component';
 
 @NgModule({
   declarations: [
     Doc090202Component,
     Doc09020206Component,
     Doc0902020601Component,
+    Doc0902020602Component,
   ],
   imports: [Doc0902RoutingModule, SharedModule],
 })
