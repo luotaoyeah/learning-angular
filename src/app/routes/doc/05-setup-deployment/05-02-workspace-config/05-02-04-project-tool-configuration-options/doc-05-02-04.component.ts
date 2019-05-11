f67f9f13618631a7d480f0074a8e57e3daef91c4
luotao <luotaoyeah@gmail.com>
2019-05-11 14:31:15 +0800

    feat(doc.setup.config): project tool configuration options

---
+++ src/app/routes/doc/05-setup-deployment/05-02-workspace-config/05-02-04-project-tool-configuration-options/doc-05-02-04.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+/*
+ * https://angular.io/guide/workspace-config#project-tool-configuration-options
+ */
+@Component({
+  selector: 'app-doc-05-02-04',
+  templateUrl: './doc-05-02-04.component.html',
+})
+export class Doc050204Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
--- src/app/routes/doc/05-setup-deployment/05-02-workspace-config/doc-05-02.module.ts
+++ src/app/routes/doc/05-setup-deployment/05-02-workspace-config/doc-05-02.module.ts
@@ -7,6 +7,7 @@ import { Doc0502Component } from './doc-05-02.component';
 import { Doc050201Component } from './05-02-01-overall-json-structure/doc-05-02-01.component';
 import { Doc050202Component } from './05-02-02-project-configuration-options/doc-05-02-02.component';
 import { Doc050203Component } from './05-02-03-generation-schematics/doc-05-02-03.component';
+import { Doc050204Component } from './05-02-04-project-tool-configuration-options/doc-05-02-04.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { Doc050203Component } from './05-02-03-generation-schematics/doc-05-02-0
     Doc050201Component,
     Doc050202Component,
     Doc050203Component,
+    Doc050204Component,
   ],
   imports: [CommonModule, FormsModule, SharedModule, Doc0502RoutingModule],
 })
