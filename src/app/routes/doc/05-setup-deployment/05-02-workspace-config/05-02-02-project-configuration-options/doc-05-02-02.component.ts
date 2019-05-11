4f1d62f6265f6a272a8f6e2d3868fbb52888ca05
luotao <luotaoyeah@gmail.com>
2019-05-11 11:24:08 +0800

    feat(doc.setup.config): project configuration options

---
+++ src/app/routes/doc/05-setup-deployment/05-02-workspace-config/05-02-02-project-configuration-options/doc-05-02-02.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+/*
+ * https://angular.io/guide/workspace-config#project-configuration-options
+ */
+@Component({
+  selector: 'app-doc-05-02-02',
+  templateUrl: './doc-05-02-02.component.html',
+})
+export class Doc050202Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
--- src/app/routes/doc/05-setup-deployment/05-02-workspace-config/doc-05-02.module.ts
+++ src/app/routes/doc/05-setup-deployment/05-02-workspace-config/doc-05-02.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '@shared';
 import { Doc0502RoutingModule } from './doc-05-02-routing.module';
 import { Doc0502Component } from './doc-05-02.component';
 import { Doc050201Component } from './05-02-01-overall-json-structure/doc-05-02-01.component';
+import { Doc050202Component } from './05-02-02-project-configuration-options/doc-05-02-02.component';
 
 @NgModule({
-  declarations: [Doc0502Component, Doc050201Component],
+  declarations: [Doc0502Component, Doc050201Component, Doc050202Component],
   imports: [CommonModule, FormsModule, SharedModule, Doc0502RoutingModule],
 })
 export class Doc0502Module {}
