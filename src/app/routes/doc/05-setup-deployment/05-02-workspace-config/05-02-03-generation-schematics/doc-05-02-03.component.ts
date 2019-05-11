e015c000588fbff9bb97816c55fd03edad825dd3
luotao <luotaoyeah@gmail.com>
2019-05-11 12:05:45 +0800

    feat(doc.setup.config): generation schematics

---
+++ src/app/routes/doc/05-setup-deployment/05-02-workspace-config/05-02-03-generation-schematics/doc-05-02-03.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+/*
+ * https://angular.io/guide/workspace-config#generation-schematics
+ */
+@Component({
+  selector: 'app-doc-05-02-03',
+  templateUrl: './doc-05-02-03.component.html',
+})
+export class Doc050203Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
--- src/app/routes/doc/05-setup-deployment/05-02-workspace-config/doc-05-02.module.ts
+++ src/app/routes/doc/05-setup-deployment/05-02-workspace-config/doc-05-02.module.ts
@@ -6,9 +6,15 @@ import { Doc0502RoutingModule } from './doc-05-02-routing.module';
 import { Doc0502Component } from './doc-05-02.component';
 import { Doc050201Component } from './05-02-01-overall-json-structure/doc-05-02-01.component';
 import { Doc050202Component } from './05-02-02-project-configuration-options/doc-05-02-02.component';
+import { Doc050203Component } from './05-02-03-generation-schematics/doc-05-02-03.component';
 
 @NgModule({
-  declarations: [Doc0502Component, Doc050201Component, Doc050202Component],
+  declarations: [
+    Doc0502Component,
+    Doc050201Component,
+    Doc050202Component,
+    Doc050203Component,
+  ],
   imports: [CommonModule, FormsModule, SharedModule, Doc0502RoutingModule],
 })
 export class Doc0502Module {}
