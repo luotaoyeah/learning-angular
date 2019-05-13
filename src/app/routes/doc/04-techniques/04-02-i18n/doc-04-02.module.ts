9016237bd313aeb820f3bc8069aa077e64fa95c9
luotao <luotaoyeah@gmail.com>
2019-05-13 21:04:07 +0800

    feat(doc.techniques.i18n): setting up the locale of your app

--- src/app/routes/doc/04-techniques/04-02-i18n/doc-04-02.module.ts
+++ src/app/routes/doc/04-techniques/04-02-i18n/doc-04-02.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '@shared';
 import { Doc0402RoutingModule } from './doc-04-02-routing.module';
 import { Doc0402Component } from './doc-04-02.component';
 import { Doc040201Component } from './04-02-01-angular-and-i18n/doc-04-02-01.component';
+import { Doc040202Component } from './04-02-02-setting-up-the-locale-of-your-app/doc-04-02-02.component';
 
 @NgModule({
-  declarations: [Doc0402Component, Doc040201Component],
+  declarations: [Doc0402Component, Doc040201Component, Doc040202Component],
   imports: [Doc0402RoutingModule, SharedModule],
 })
 export class Doc0402Module {}
--- src/app/routes/doc/doc.module.ts
+++ src/app/routes/doc/doc.module.ts
@@ -3,9 +3,10 @@ import { Doc03Module } from './03-fundamental/doc-03.module';
 import { Doc02Module } from './02-tutorial/doc-02.module';
 import { Doc05Module } from './05-setup-deployment/doc-05.module';
 import { Doc09Module } from './09-api/doc-09.module';
+import { Doc04Module } from './04-techniques/doc-04.module';
 
 @NgModule({
-  imports: [Doc02Module, Doc03Module, Doc05Module, Doc09Module],
-  exports: [Doc02Module, Doc03Module, Doc05Module, Doc09Module],
+  imports: [Doc02Module, Doc03Module, Doc04Module, Doc05Module, Doc09Module],
+  exports: [Doc02Module, Doc03Module, Doc04Module, Doc05Module, Doc09Module],
 })
 export class DocModule {}
