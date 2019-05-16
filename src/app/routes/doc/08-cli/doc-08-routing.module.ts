136940f86083e847c997845703441bd595692af9
luotao <luotaoyeah@gmail.com>
2019-05-16 21:40:15 +0800

    feat(doc.cli): ng add

--- src/app/routes/doc/08-cli/doc-08-routing.module.ts
+++ src/app/routes/doc/08-cli/doc-08-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Doc0801Component } from './08-01-overview/doc-08-01.component';
+import { Doc0802Component } from './08-02-add/doc-08-02.component';
 
 const routes: Routes = [
   {
     path: '08/01',
     component: Doc0801Component,
+  },
+  {
+    path: '08/02',
+    component: Doc0802Component,
   },
 ];
 
--- src/app/routes/doc/08-cli/doc-08.module.ts
+++ src/app/routes/doc/08-cli/doc-08.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Doc08RoutingModule } from './doc-08-routing.module';
 import { Doc0801Component } from './08-01-overview/doc-08-01.component';
+import { Doc0802Component } from './08-02-add/doc-08-02.component';
+import { SharedModule } from '@shared';
 
 @NgModule({
-  declarations: [Doc0801Component],
-  imports: [CommonModule, Doc08RoutingModule],
+  declarations: [Doc0801Component, Doc0802Component],
+  imports: [CommonModule, Doc08RoutingModule, SharedModule],
   exports: [],
 })
 export class Doc08Module {}
