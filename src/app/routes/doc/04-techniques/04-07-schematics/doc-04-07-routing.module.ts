aa2636cc432a72c58e5cf747bd73b7a9dabce95d
luotao <luotaoyeah@gmail.com>
2019-05-23 15:36:18 +0800

    feat(techniques.schematics): schematics CLI

--- src/app/routes/doc/04-techniques/04-07-schematics/doc-04-07-routing.module.ts
+++ src/app/routes/doc/04-techniques/04-07-schematics/doc-04-07-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Doc040703Component } from './04-07-03-schematics-for-libraries/doc-04-07-03.component';
 import { Doc040701Component } from './04-07-01-schematics-overview/doc-04-07-01.component';
+import { Doc040702Component } from './04-07-02-authoring-schematics/doc-04-07-02.component';
 
 const routes: Routes = [
   {
     path: '04/07/01',
     component: Doc040701Component,
+  },
+  {
+    path: '04/07/02',
+    component: Doc040702Component,
   },
   {
     path: '04/07/03',
--- src/app/routes/doc/04-techniques/04-07-schematics/doc-04-07.module.ts
+++ src/app/routes/doc/04-techniques/04-07-schematics/doc-04-07.module.ts
@@ -3,9 +3,10 @@ import { SharedModule } from '@shared';
 import { Doc0407RoutingModule } from './doc-04-07-routing.module';
 import { Doc040703Component } from './04-07-03-schematics-for-libraries/doc-04-07-03.component';
 import { Doc040701Component } from './04-07-01-schematics-overview/doc-04-07-01.component';
+import { Doc040702Component } from './04-07-02-authoring-schematics/doc-04-07-02.component';
 
 @NgModule({
-  declarations: [Doc040701Component, Doc040703Component],
+  declarations: [Doc040701Component, Doc040702Component, Doc040703Component],
   imports: [Doc0407RoutingModule, SharedModule],
 })
 export class Doc0407Module {}
