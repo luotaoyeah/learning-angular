9db3640011ce6366196f5a93a15a70d798757921
luotao <luotaoyeah@gmail.com>
2019-05-24 15:01:14 +0800

    feat(techniques.schematics): providing generation support

---
+++ src/app/routes/doc/04-techniques/04-07-schematics/04-07-03-schematics-for-libraries/04-07-03-02-providing-generation-support/doc-04-07-03-02.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+/*
+ * https://angular.io/guide/schematics-for-libraries#providing-generation-support
+ */
+@Component({
+  selector: 'app-doc-04-07-03-02',
+  templateUrl: './doc-04-07-03-02.component.html',
+})
+export class Doc04070302Component implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
--- src/app/routes/doc/04-techniques/04-07-schematics/doc-04-07.module.ts
+++ src/app/routes/doc/04-techniques/04-07-schematics/doc-04-07.module.ts
@@ -4,9 +4,15 @@ import { Doc0407RoutingModule } from './doc-04-07-routing.module';
 import { Doc040703Component } from './04-07-03-schematics-for-libraries/doc-04-07-03.component';
 import { Doc040701Component } from './04-07-01-schematics-overview/doc-04-07-01.component';
 import { Doc040702Component } from './04-07-02-authoring-schematics/doc-04-07-02.component';
+import { Doc04070302Component } from './04-07-03-schematics-for-libraries/04-07-03-02-providing-generation-support/doc-04-07-03-02.component';
 
 @NgModule({
-  declarations: [Doc040701Component, Doc040702Component, Doc040703Component],
+  declarations: [
+    Doc040701Component,
+    Doc040702Component,
+    Doc04070302Component,
+    Doc040703Component,
+  ],
   imports: [Doc0407RoutingModule, SharedModule],
 })
 export class Doc0407Module {}
