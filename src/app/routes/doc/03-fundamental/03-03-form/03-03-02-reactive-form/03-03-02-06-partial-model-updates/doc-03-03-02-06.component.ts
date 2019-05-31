e3182c427c7d2869946e16c455eab663487e0184
luotao <luotaoyeah@gmail.com>
2019-05-31 15:44:00 +0800

    refactor(doc.guide.form.reactive-form): partial model updates

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-06-partial-model-updates/doc-03-03-02-06.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-06-partial-model-updates/doc-03-03-02-06.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+/*
+ * https://angular.io/guide/reactive-forms#partial-model-updates
+ */
 @Component({
   selector: 'app-doc-03-03-02-06',
   templateUrl: './doc-03-03-02-06.component.html',
 })
-export class Doc03030206Component implements OnInit {
+export class Doc03030206Component {
   public profileFormGroup: FormGroup = new FormGroup({
     name: new FormControl('foo'),
     address: new FormGroup({
@@ -14,13 +17,11 @@ export class Doc03030206Component implements OnInit {
     }),
   });
 
-  public ngOnInit() {}
-
   public setValue() {
     /*
-     * 可以调用 FormGroup.setValue() 方法更新数据，
-     * 此时，参数对象的数据格式必须严格等于 FormGroup 对象的数据格式，
-     * 如下，如果我只是想更新 address.street 属性，但是其他的属性（如：name）也必须提供
+     * 可以调用 FormGroup.setValue() 方法更新数据,
+     * 此时, 参数对象的数据格式必须严格等于 FormGroup 对象的数据格式,
+     * 如下, 如果我只是想更新 address.street 属性, 但是其他的属性（如：name）也必须提供
      */
     this.profileFormGroup.setValue({
       name: 'foo',
@@ -32,25 +33,25 @@ export class Doc03030206Component implements OnInit {
   }
 
   /*
-   * 可以调用 FormGroup.patchValue() 方法更新数据，
-   * 此时，参数对象中只需要包含需要更新的属性，其他属性可以忽略，
-   * 如下，只更新了 address.street 属性
+   * 可以调用 FormGroup.patchValue() 方法更新数据,
+   * 此时, 参数对象中只需要包含需要更新的属性, 其他属性可以忽略,
+   * 如下, 只更新了 address.zip 属性, 其他的属性值会保持不变
    */
   public patchValue() {
     this.profileFormGroup.patchValue({
       address: {
-        street: 'BAR',
+        zip: 'BAZ',
       },
     });
   }
 
   public reset() {
-    this.profileFormGroup = new FormGroup({
-      name: new FormControl('foo'),
-      address: new FormGroup({
-        street: new FormControl('bar'),
-        zip: new FormControl('baz'),
-      }),
+    this.profileFormGroup.setValue({
+      name: 'foo',
+      address: {
+        street: 'bar',
+        zip: 'baz',
+      },
     });
   }
 }
