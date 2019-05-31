045af199dcb41a3fcabd77b93ea205e3752490ea
luotao <luotaoyeah@gmail.com>
2019-05-31 15:27:59 +0800

    refactor(doc.guide.form.reactive-form): creating nested form groups

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-05-creating-nested-form-group/doc-03-03-02-05.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-05-creating-nested-form-group/doc-03-03-02-05.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+/*
+ * https://angular.io/guide/reactive-forms#creating-nested-form-groups
+ */
 @Component({
   selector: 'app-doc-03-03-02-05',
   templateUrl: './doc-03-03-02-05.component.html',
 })
-export class Doc03030205Component implements OnInit {
+export class Doc03030205Component {
   public profileFormGroup: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    firstName: new FormControl('TAO'),
+    lastName: new FormControl('LUO'),
     /*
-     * FormGroup 可以嵌套，也就是说 FormGroup 下面可以同时包含 FormControl 和 FormGroup
+     * FormGroup 可以嵌套, 也就是说 FormGroup 下面可以同时包含 FormControl 和 FormGroup
      */
     address: new FormGroup({
-      street: new FormControl(''),
-      city: new FormControl(''),
-      state: new FormControl(''),
-      zip: new FormControl(''),
+      street: new FormControl('some street'),
+      city: new FormControl('chengdu'),
+      state: new FormControl('sichuan'),
+      zip: new FormControl('600000'),
     }),
   });
 
-  public ngOnInit() {}
-
   public onSubmit() {
-    console.log(this.profileFormGroup.value);
+    console.log(
+      `[${Doc03030205Component.name}]\n`,
+      this.profileFormGroup.value,
+    );
   }
 }
