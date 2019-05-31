600b9c2a3082e5320ebd57d3e3d13b353f32aba3
luotao <luotaoyeah@gmail.com>
2019-05-31 14:46:19 +0800

    refactor(doc.guide.form.reactive-form): replacing a form control value

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-03-managing-control-values/03-03-02-03-02-replacing-a-form-control-value/doc-03-03-02-03-02.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-03-managing-control-values/03-03-02-03-02-replacing-a-form-control-value/doc-03-03-02-03-02.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 
+/*
+ * https://angular.io/guide/reactive-forms#replacing-a-form-control-value
+ */
 @Component({
   selector: 'app-doc-03-03-02-03-02',
   templateUrl: './doc-03-03-02-03-02.component.html',
 })
-export class Doc0303020302Component implements OnInit {
-  public nameFormControl: FormControl = new FormControl('');
+export class Doc0303020302Component {
+  public nameFormControl: FormControl = new FormControl('TOM');
 
-  public ngOnInit() {}
-
-  /**
-   * 可以直接调用 FormControl.setValue() 方法，更新 form control 的数据，
-   * 在 reactive form 中，form control 是 source of true，
-   * 当 form control 的数据更新之后，对应的 input element 的值会自动更新
+  /*
+   * 可以直接调用 FormControl.setValue() 方法, 更新 FormControl 对象的数据,
+   * 当 FormControl 对象的数据更新之后, 对应的 <input /> 的值会自动更新
    */
   public updateName(): void {
     this.nameFormControl.setValue('OWEN');
