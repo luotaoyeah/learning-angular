927ad7dfe061d5263ce6df06201b417a3db69ad1
luotao <luotaoyeah@gmail.com>
2019-05-31 16:05:04 +0800

    refactor(doc.guide.form.reactive-form): simple form validation

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-08-simple-form-validation/doc-03-03-02-08.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-08-simple-form-validation/doc-03-03-02-08.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+/*
+ * https://angular.io/guide/reactive-forms#simple-form-validation
+ */
 @Component({
   selector: 'app-doc-03-03-02-08',
   templateUrl: './doc-03-03-02-08.component.html',
 })
-export class Doc03030208Component implements OnInit {
+export class Doc03030208Component {
   /*
-   * angular 内置了一些常用的 validator function，
-   * 可以通过 Validators 的静态方法来使用它们
+   * angular 内置了一些常用的 validator functions,
+   * 可以通过 {@link Validators} 的静态方法来使用它们
    */
-  public profileFormGroup: FormGroup = this.formBuilder.group({
-    name: [''],
-    address: this.formBuilder.group({
-      street: ['', [Validators.required]],
-      zip: [''],
+  public profileFormGroup: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    address: new FormGroup({
+      street: new FormControl('', {
+        validators: [Validators.required],
+      }),
+      zip: new FormControl(''),
     }),
   });
 
-  constructor(private formBuilder: FormBuilder) {}
+  public handleSubmit() {
+    if (this.profileFormGroup.invalid) {
+      return;
+    }
 
-  public ngOnInit() {}
+    console.log(
+      `[${Doc03030208Component.name}]\n`,
+      this.profileFormGroup.value,
+    );
+  }
 }
