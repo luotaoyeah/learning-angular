7492fa6f9f5727e259910300b35a93077c3deeed
luotao <luotaoyeah@gmail.com>
2019-05-31 15:03:04 +0800

    refactor(doc.guide.form.reactive-form): grouping form controls

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-04-grouping-form-controls/doc-03-03-02-04.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-02-reactive-form/03-03-02-04-grouping-form-controls/doc-03-03-02-04.component.ts
@@ -1,24 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+/*
+ * https://angular.io/guide/reactive-forms#grouping-form-controls
+ */
 @Component({
   selector: 'app-doc-03-03-02-04',
   templateUrl: './doc-03-03-02-04.component.html',
 })
-export class Doc03030204Component implements OnInit {
+export class Doc03030204Component {
   /*
-   * FormGroup 表示一组 FormControl，通常对应一个 form element，
-   * FormGroup 对它的 FormControl 进行统一管理，
+   * FormGroup 表示一组 FormControl, 通常对应一个 <form/> 元素,
+   * FormGroup 对它的 FormControl 进行统一管理,
    * FormGroup 跟 FormControl 拥有相同的属性和方法
    */
   public profileFormGroup: FormGroup = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
+    firstName: new FormControl('TAO'),
+    lastName: new FormControl('LUO'),
   });
 
-  public ngOnInit() {}
-
   public onSubmit() {
-    console.log(this.profileFormGroup.value);
+    console.log(
+      `[${Doc03030204Component.name}]\n`,
+      this.profileFormGroup.value,
+    );
   }
 }
