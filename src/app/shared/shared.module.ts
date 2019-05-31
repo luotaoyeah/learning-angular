76771f5c3ec3f4ce4e1ec552d30cdcfd20a04c1d
luotao <luotaoyeah@gmail.com>
2019-05-31 14:29:29 +0800

    refactor(doc.guide.form.reactive-form): getting started

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,48 +2,41 @@ import { NgModule, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-// delon
 import { AlainThemeModule } from '@delon/theme';
 import { DelonABCModule } from '@delon/abc';
 import { DelonACLModule } from '@delon/acl';
 import { DelonFormModule } from '@delon/form';
-// i18n
 import { TranslateModule } from '@ngx-translate/core';
-// #region third libs
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { CountdownModule } from 'ngx-countdown';
 
 // tslint:disable-next-line:no-any
 const THIRDMODULES: Array<any> = [NgZorroAntdModule, CountdownModule];
-// #endregion
 
-/*
- * your componets & directives
- */
+// region your componets & directives
 
 // tslint:disable-next-line:no-any
 const COMPONENTS: Array<any> = [];
 // tslint:disable-next-line:no-any
 const DIRECTIVES: Array<Type<any> | any[]> = [];
 
+// endregion
+
 @NgModule({
+  declarations: [...COMPONENTS, ...DIRECTIVES],
   imports: [
     CommonModule,
     FormsModule,
     RouterModule,
-    // 使用 reactive form 之前，必须引入 ReactiveFormsModule
+    /*
+     * 要使用 reactive form, 则必须引入 ReactiveFormsModule
+     */
     ReactiveFormsModule,
     AlainThemeModule.forChild(),
     DelonABCModule,
     DelonACLModule,
     DelonFormModule,
-    // third libs
     ...THIRDMODULES,
-  ],
-  declarations: [
-    // your components
-    ...COMPONENTS,
-    ...DIRECTIVES,
   ],
   exports: [
     CommonModule,
@@ -54,11 +47,8 @@ const DIRECTIVES: Array<Type<any> | any[]> = [];
     DelonABCModule,
     DelonACLModule,
     DelonFormModule,
-    // i18n
     TranslateModule,
-    // third libs
     ...THIRDMODULES,
-    // your components
     ...COMPONENTS,
     ...DIRECTIVES,
   ],
