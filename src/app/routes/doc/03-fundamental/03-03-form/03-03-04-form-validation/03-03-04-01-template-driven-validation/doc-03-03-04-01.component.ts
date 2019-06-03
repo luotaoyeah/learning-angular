5140d9468849ba8f1faa2227fab88a3b2e2e420a
luotao <luotaoyeah@gmail.com>
2019-06-03 12:04:52 +0800

    refactor(doc.guide.form.form-validation): template-driven validation

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-01-template-driven-validation/doc-03-03-04-01.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-01-template-driven-validation/doc-03-03-04-01.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/form-validation#template-driven-validation
+ */
 @Component({
   selector: 'app-doc-03-03-04-01',
   templateUrl: './doc-03-03-04-01.component.html',
 })
-export class Doc03030401Component implements OnInit {
+export class Doc03030401Component {
   public name: string = '';
-
-  public ngOnInit() {}
 }
