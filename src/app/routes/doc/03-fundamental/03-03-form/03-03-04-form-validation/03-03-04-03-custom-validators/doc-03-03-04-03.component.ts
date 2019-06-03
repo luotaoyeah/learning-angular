b9ef60897073d62cc48d020398ad91008681a81e
luotao <luotaoyeah@gmail.com>
2019-06-03 13:38:11 +0800

    refactor(doc.guide.form.form-validation): custom validators

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-03-custom-validators/doc-03-03-04-03.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-03-custom-validators/doc-03-03-04-03.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { forbiddenNameValidator } from './validator/forbidden-name-validator';
+import { forbiddenNameValidator } from './validators/forbidden-name.validator';
 
+/*
+ * https://angular.io/guide/form-validation#custom-validators
+ */
 @Component({
   selector: 'app-doc-03-03-04-03',
   templateUrl: './doc-03-03-04-03.component.html',
 })
-export class Doc03030403Component implements OnInit {
+export class Doc03030403Component {
   public formGroup: FormGroup = new FormGroup({
     name: new FormControl('', {
       /*
-       * custom validator function 如何在 reactive form 中使用？
+       * custom validator function 如何在 reactive form 中使用 ?
        * 直接跟 built-in validator function 一样使用
        */
       validators: [Validators.required, forbiddenNameValidator(/foo/)],
@@ -19,5 +22,7 @@ export class Doc03030403Component implements OnInit {
 
   public name: string = '';
 
-  public ngOnInit() {}
+  public get nameControl(): FormControl {
+    return this.formGroup.get('name') as FormControl;
+  }
 }
--- src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/doc-03-03-04.module.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/doc-03-03-04.module.ts
@@ -3,7 +3,7 @@ import { Doc030304Component } from './doc-03-03-04.component';
 import { Doc03030401Component } from './03-03-04-01-template-driven-validation/doc-03-03-04-01.component';
 import { Doc03030402Component } from './03-03-04-02-reactive-form-validation/doc-03-03-04-02.component';
 import { Doc03030403Component } from './03-03-04-03-custom-validators/doc-03-03-04-03.component';
-import { ForbiddenNameDirective } from './03-03-04-03-custom-validators/validator/forbidden-name.directive';
+import { ForbiddenNameDirective } from './03-03-04-03-custom-validators/validators/forbidden-name.directive';
 import { Doc03030404Component } from './03-03-04-04-cross-field-validation/doc-03-03-04-04.component';
 import { PasswordDirective } from './03-03-04-04-cross-field-validation/validator/password.directive';
 import { Doc03030405Component } from './03-03-04-05-async-validation/doc-03-03-04-05.component';
