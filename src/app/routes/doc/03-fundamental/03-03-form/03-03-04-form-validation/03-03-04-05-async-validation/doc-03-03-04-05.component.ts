dc190e4aa0f5f9ffd33182ab0115f195474227cb
luotao <luotaoyeah@gmail.com>
2019-06-03 14:10:24 +0800

    refactor(doc.guide.form.form-validation): async validation

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-05-async-validation/doc-03-03-04-05.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-05-async-validation/doc-03-03-04-05.component.ts
@@ -1,26 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { UniqueValidator } from './validator/unique-validator';
+import { UniqueValidator } from './validators/unique.validator';
 
+/*
+ * https://angular.io/guide/form-validation#async-validation
+ */
 @Component({
   selector: 'app-doc-03-03-04-05',
   templateUrl: './doc-03-03-04-05.component.html',
 })
-export class Doc03030405Component implements OnInit {
+export class Doc03030405Component {
   public formGroup: FormGroup = new FormGroup({
     name: new FormControl('', {
       /*
-       * 考虑到 async validator function 的性能影响，
-       * 对于比较耗费时间的 async validator，可以将验证策略设置为 blur，
-       * 即当表单失去焦点时，才进行验证，
+       * 考虑到 async validator function 的性能影响,
+       * 对于比较耗费时间的 async validator, 可以将验证策略设置为 blur,
+       * 即当表单失去焦点时, 才进行验证,
        * 默认是在每次 change 的时候都会进行验证
        */
       updateOn: 'blur',
-      asyncValidators: [this.uniqueValidator.validate],
+      asyncValidators: [
+        this.uniqueValidator.validate.bind(this.uniqueValidator),
+      ],
     }),
   });
 
   constructor(private uniqueValidator: UniqueValidator) {}
 
-  public ngOnInit() {}
+  get nameControl(): FormControl {
+    return this.formGroup.get('name') as FormControl;
+  }
 }
--- src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-05-async-validation/validators/unique.validator.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-05-async-validation/validators/unique.validator.ts
@@ -10,12 +10,18 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class UniqueValidator implements AsyncValidator {
+  /**
+   *
+   * @param control the form model
+   */
   public validate(
     control: AbstractControl,
   ): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
     return new Observable((subscriber: Subscriber<ValidationErrors | null>) => {
       setTimeout(() => {
-        subscriber.next(control.value === 'foo' ? { unique: true } : null);
+        subscriber.next(
+          control.value === 'foo' ? { unique: { value: control.value } } : null,
+        );
 
         /*
          * async validator function 返回的 observable 必须调用 complete() 方法结束
