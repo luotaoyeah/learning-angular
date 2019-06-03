21611f9e68bfa8b4c8161447f26ff685888fae54
luotao <luotaoyeah@gmail.com>
2019-06-03 13:13:20 +0800

    refactor(doc.guide.form.form-validation): reactive form validation

--- src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-02-reactive-form-validation/doc-03-03-04-02.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-form/03-03-04-form-validation/03-03-04-02-reactive-form-validation/doc-03-03-04-02.component.ts
@@ -1,20 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+/*
+ * https://angular.io/guide/form-validation#reactive-form-validation
+ */
 @Component({
   selector: 'app-doc-03-03-04-02',
   templateUrl: './doc-03-03-04-02.component.html',
 })
-export class Doc03030402Component implements OnInit {
+export class Doc03030402Component {
   public formGroup: FormGroup = new FormGroup({
     name: new FormControl('', {
       validators: [Validators.required, Validators.minLength(4)],
     }),
   });
 
-  get formGroupName(): FormControl {
+  get nameControl(): FormControl {
     return this.formGroup.get(['name']) as FormControl;
   }
-
-  public ngOnInit() {}
 }
