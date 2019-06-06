a2170ed25e2913e1b4477e7eb24d43d8153a025f
luotao <luotaoyeah@gmail.com>
2019-06-06 14:51:54 +0800

    feat(pkgs.angular-gridster2): home

--- src/app/routes/pkgs/angular-gridster2/01-home/x-01.component.ts
+++ src/app/routes/pkgs/angular-gridster2/01-home/x-01.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import {
+  CompactType,
+  DisplayGrid,
+  GridsterConfig,
+  GridsterItem,
+  GridType,
+} from 'angular-gridster2';
 
 /*
  * https://tiberiuzuld.github.io/angular-gridster2/
@@ -6,9 +13,129 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-x-01',
   templateUrl: './x-01.component.html',
+  styleUrls: ['./x-01.component.less'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class X01Component implements OnInit {
-  constructor() {}
+  public options: GridsterConfig = {};
+  public dashboard: Array<GridsterItem> = [];
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    this.options = {
+      gridType: GridType.Fit,
+      compactType: CompactType.None,
+      margin: 10,
+      outerMargin: true,
+      outerMarginTop: null,
+      outerMarginRight: null,
+      outerMarginBottom: null,
+      outerMarginLeft: null,
+      useTransformPositioning: true,
+      mobileBreakpoint: 640,
+      minCols: 1,
+      maxCols: 100,
+      minRows: 1,
+      maxRows: 100,
+      maxItemCols: 100,
+      minItemCols: 1,
+      maxItemRows: 100,
+      minItemRows: 1,
+      maxItemArea: 2500,
+      minItemArea: 1,
+      defaultItemCols: 1,
+      defaultItemRows: 1,
+      fixedColWidth: 105,
+      fixedRowHeight: 105,
+      keepFixedHeightInMobile: false,
+      keepFixedWidthInMobile: false,
+      scrollSensitivity: 10,
+      scrollSpeed: 20,
+      enableEmptyCellClick: false,
+      enableEmptyCellContextMenu: false,
+      enableEmptyCellDrop: false,
+      enableEmptyCellDrag: false,
+      emptyCellDragMaxCols: 50,
+      emptyCellDragMaxRows: 50,
+      ignoreMarginInRow: false,
+      draggable: {
+        enabled: true,
+      },
+      resizable: {
+        enabled: true,
+      },
+      swap: false,
+      pushItems: true,
+      disablePushOnDrag: false,
+      disablePushOnResize: false,
+      pushDirections: { north: true, east: true, south: true, west: true },
+      pushResizeItems: false,
+      displayGrid: DisplayGrid.Always,
+      disableWindowResize: false,
+      disableWarnings: false,
+      scrollToNewItems: false,
+    };
+
+    this.dashboard = [
+      { cols: 2, rows: 1, y: 0, x: 0 },
+      { cols: 2, rows: 2, y: 0, x: 2, hasContent: true },
+      { cols: 1, rows: 1, y: 0, x: 4 },
+      { cols: 1, rows: 1, y: 2, x: 5 },
+      { cols: 1, rows: 1, y: 1, x: 0 },
+      { cols: 1, rows: 1, y: 1, x: 0 },
+      {
+        cols: 2,
+        rows: 2,
+        y: 3,
+        x: 5,
+        minItemRows: 2,
+        minItemCols: 2,
+        label: 'Min rows & cols = 2',
+      },
+      {
+        cols: 2,
+        rows: 2,
+        y: 2,
+        x: 0,
+        maxItemRows: 2,
+        maxItemCols: 2,
+        label: 'Max rows & cols = 2',
+      },
+      {
+        cols: 2,
+        rows: 1,
+        y: 2,
+        x: 2,
+        dragEnabled: true,
+        resizeEnabled: true,
+        label: 'Drag&Resize Enabled',
+      },
+      {
+        cols: 1,
+        rows: 1,
+        y: 2,
+        x: 4,
+        dragEnabled: false,
+        resizeEnabled: false,
+        label: 'Drag&Resize Disabled',
+      },
+      { cols: 1, rows: 1, y: 2, x: 6 },
+    ];
+  }
+
+  public changedOptions() {
+    if (this.options.api && this.options.api.optionsChanged) {
+      this.options.api.optionsChanged();
+    }
+  }
+
+  // tslint:disable-next-line:no-any
+  public removeItem($event: any, item: any) {
+    $event.preventDefault();
+    $event.stopPropagation();
+    this.dashboard.splice(this.dashboard.indexOf(item), 1);
+  }
+
+  public addItem() {
+    this.dashboard.push({ x: 0, y: 0, cols: 1, rows: 1 });
+  }
 }
--- src/app/routes/pkgs/angular-gridster2/angular-gridster2.module.ts
+++ src/app/routes/pkgs/angular-gridster2/angular-gridster2.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { X01Component } from './01-home/x-01.component';
 import { AngularGridster2RoutingModule } from './angular-gridster2-routing.module';
+import { GridsterModule } from 'angular-gridster2';
+import { SharedModule } from '@app/shared';
 
 @NgModule({
   declarations: [X01Component],
-  imports: [AngularGridster2RoutingModule],
+  imports: [AngularGridster2RoutingModule, SharedModule, GridsterModule],
 })
 export class AngularGridster2Module {}
