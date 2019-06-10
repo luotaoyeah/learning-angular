d25de8a5fa28dd5c5365b2a5dc8b6c04e3c693f8
luotao <luotaoyeah@gmail.com>
2019-06-10 21:07:15 +0800

    refactor(doc.structural-directives): the asterisk prefix

--- src/app/routes/doc/03-fundamental/03-03-components-templates/03-03-10-structural-directives/03-03-10-03-the-asterisk-prefix/doc-03-03-10-03.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-components-templates/03-03-10-structural-directives/03-03-10-03-the-asterisk-prefix/doc-03-03-10-03.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/structural-directives#the-asterisk--prefix
+ */
 @Component({
   selector: 'app-doc-03-03-10-03',
   templateUrl: './doc-03-03-10-03.component.html',
 })
-export class Doc03031003Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc03031003Component {}
