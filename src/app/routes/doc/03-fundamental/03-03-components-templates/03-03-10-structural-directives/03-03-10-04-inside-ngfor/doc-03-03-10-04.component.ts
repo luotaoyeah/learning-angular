b9f8ceb0e2285c85c1e62a4cc87cc01a83a5d838
luotao <luotaoyeah@gmail.com>
2019-06-10 21:25:55 +0800

    refactor(doc.structural-directives): inside *ngFor

--- src/app/routes/doc/03-fundamental/03-03-components-templates/03-03-10-structural-directives/03-03-10-04-inside-ngfor/doc-03-03-10-04.component.ts
+++ src/app/routes/doc/03-fundamental/03-03-components-templates/03-03-10-structural-directives/03-03-10-04-inside-ngfor/doc-03-03-10-04.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/structural-directives#inside-ngfor
+ */
 @Component({
   selector: 'app-doc-03-03-10-04',
   templateUrl: './doc-03-03-10-04.component.html',
-  styles: ['.odd{color:#ff0000;}'],
+  styles: ['.odd{ color: #ff0000; }'],
 })
-export class Doc03031004Component implements OnInit {
+export class Doc03031004Component {
   public heroes: Array<{ id: number; name: string }> = [
     { id: 1, name: 'FOO' },
     { id: 2, name: 'BAR' },
     { id: 3, name: 'BAZ' },
   ];
-
-  public ngOnInit() {}
 
   public trackById(hero: { id: number; name: string }): number {
     return hero.id;
