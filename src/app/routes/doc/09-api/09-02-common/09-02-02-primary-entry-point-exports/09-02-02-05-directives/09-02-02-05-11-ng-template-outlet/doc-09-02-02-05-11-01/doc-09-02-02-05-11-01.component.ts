d4d21395f64114307569b9b83f6b6a9870043398
luotao <luotaoyeah@gmail.com>
2019-06-11 19:42:19 +0800

    feat(doc.api): NgTemplateOutlet: ngTemplateOutletContext

---
+++ src/app/routes/doc/09-api/09-02-common/09-02-02-primary-entry-point-exports/09-02-02-05-directives/09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11-01/doc-09-02-02-05-11-01.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+/*
+ * https://angular.io/api/common/NgTemplateOutlet
+ */
+@Component({
+  selector: 'app-doc-09-02-02-05-11-01',
+  templateUrl: './doc-09-02-02-05-11-01.component.html',
+})
+export class Doc090202051101Component {}
--- src/app/routes/doc/09-api/09-02-common/09-02-02-primary-entry-point-exports/09-02-02-05-directives/doc-09-02-02-05.module.ts
+++ src/app/routes/doc/09-api/09-02-common/09-02-02-primary-entry-point-exports/09-02-02-05-directives/doc-09-02-02-05.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Doc09020205Component } from './doc-09-02-02-05.component';
 import { SharedModule } from '@app/shared';
-import { Doc0902020511Component } from './09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11.component';
+import { Doc090202051101Component } from './09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11-01/doc-09-02-02-05-11-01.component';
+import { Doc090202051102Component } from './09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11-02/doc-09-02-02-05-11-02.component';
 
 @NgModule({
-  declarations: [Doc09020205Component, Doc0902020511Component],
+  declarations: [
+    Doc09020205Component,
+    Doc090202051101Component,
+    Doc090202051102Component,
+  ],
   imports: [SharedModule],
   exports: [Doc09020205Component],
 })
