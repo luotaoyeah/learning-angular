f9e8a4210b185b3c410e2df56a53ea2aefacb818
luotao <luotaoyeah@gmail.com>
2019-06-11 19:52:25 +0800

    feat(doc.api): NgTemplateOutlet: *ngTemplateOutlet inside *ngFor

--- src/app/routes/doc/09-api/09-02-common/09-02-02-primary-entry-point-exports/09-02-02-05-directives/doc-09-02-02-05.module.ts
+++ src/app/routes/doc/09-api/09-02-common/09-02-02-primary-entry-point-exports/09-02-02-05-directives/doc-09-02-02-05.module.ts
@@ -3,12 +3,14 @@ import { Doc09020205Component } from './doc-09-02-02-05.component';
 import { SharedModule } from '@app/shared';
 import { Doc090202051101Component } from './09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11-01/doc-09-02-02-05-11-01.component';
 import { Doc090202051102Component } from './09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11-02/doc-09-02-02-05-11-02.component';
+import { Doc090202051103Component } from './09-02-02-05-11-ng-template-outlet/doc-09-02-02-05-11-03/doc-09-02-02-05-11-03.component';
 
 @NgModule({
   declarations: [
     Doc09020205Component,
     Doc090202051101Component,
     Doc090202051102Component,
+    Doc090202051103Component,
   ],
   imports: [SharedModule],
   exports: [Doc09020205Component],
