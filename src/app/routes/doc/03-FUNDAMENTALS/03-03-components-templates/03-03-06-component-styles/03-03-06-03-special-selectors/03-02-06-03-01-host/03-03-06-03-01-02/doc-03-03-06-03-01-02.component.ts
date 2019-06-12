00821de7a9280b241d262e675f34b8d1ab53f755
luotao <luotaoyeah@gmail.com>
2019-06-12 21:54:54 +0800

    refactor(doc.component-styles): special selectors: :host

---
+++ src/app/routes/doc/03-FUNDAMENTALS/03-03-components-templates/03-03-06-component-styles/03-03-06-03-special-selectors/03-02-06-03-01-host/03-03-06-03-01-02/doc-03-03-06-03-01-02.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/*
+ * https://angular.io/guide/component-styles#host
+ */
+@Component({
+  selector: 'app-doc-03-03-06-03-01-02',
+  templateUrl: './doc-03-03-06-03-01-02.component.html',
+  styleUrls: ['./doc-03-03-06-03-01-02.component.scss'],
+})
+export class Doc030306030102Component {}
