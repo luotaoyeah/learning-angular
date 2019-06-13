7ce853ab721d6b6bcebbf2c9d27a9cedffbee1d1
luotao <luotaoyeah@gmail.com>
2019-06-13 15:10:41 +0800

    refactor(doc.routing): RouterOutlet

---
+++ src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/03-10-01-the-basics/03-10-01-04/doc-03-10-01-04.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+/*
+ * https://angular.io/guide/router#router-outlet
+ */
+@Component({
+  selector: 'app-doc-03-10-01-04',
+  templateUrl: './doc-03-10-01-04.component.html',
+})
+export class Doc03100104Component {}
--- src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/doc-03-10.module.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/doc-03-10.module.ts
@@ -7,11 +7,13 @@ import { Doc031001Component } from './03-10-01-the-basics/doc-03-10-01.component
 import { RoutingNavigationBasicActivatedRouteComponent } from './03-10-01-the-basics/activated-route/routing-navigation-basic-activated-route.component';
 import { RoutingNavigationBasicRouterEventsComponent } from './03-10-01-the-basics/router-events/routing-navigation-basic-router-events.component';
 import { Doc03100101Component } from './03-10-01-the-basics/03-10-01-01/doc-03-10-01-01.component';
+import { Doc03100104Component } from './03-10-01-the-basics/03-10-01-04/doc-03-10-01-04.component';
 
 @NgModule({
   declarations: [
     Doc0310Component,
     Doc03100101Component,
+    Doc03100104Component,
     Doc031001NotFoundComponent,
     Doc031001Component,
     RoutingNavigationBasicActivatedRouteComponent,
