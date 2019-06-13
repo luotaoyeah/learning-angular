8b51df3f4a5dd5e5fd293d6c9137f82cd8428a49
luotao <luotaoyeah@gmail.com>
2019-06-13 15:18:07 +0800

    refactor(doc.routing): RouterLink

--- src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/doc-03-10.module.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/doc-03-10.module.ts
@@ -8,12 +8,14 @@ import { RoutingNavigationBasicActivatedRouteComponent } from './03-10-01-the-ba
 import { RoutingNavigationBasicRouterEventsComponent } from './03-10-01-the-basics/router-events/routing-navigation-basic-router-events.component';
 import { Doc03100101Component } from './03-10-01-the-basics/03-10-01-01/doc-03-10-01-01.component';
 import { Doc03100104Component } from './03-10-01-the-basics/03-10-01-04/doc-03-10-01-04.component';
+import { Doc03100105Component } from './03-10-01-the-basics/03-10-01-05/doc-03-10-01-05.component';
 
 @NgModule({
   declarations: [
     Doc0310Component,
     Doc03100101Component,
     Doc03100104Component,
+    Doc03100105Component,
     Doc031001NotFoundComponent,
     Doc031001Component,
     RoutingNavigationBasicActivatedRouteComponent,
