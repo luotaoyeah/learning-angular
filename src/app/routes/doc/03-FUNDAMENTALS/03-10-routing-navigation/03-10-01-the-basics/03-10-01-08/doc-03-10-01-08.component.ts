686f244fc6ac0120cdcf9f0bd0479a2f21e9a43a
luotao <luotaoyeah@gmail.com>
2019-06-13 17:22:06 +0800

    refactor(doc.routing): Router.events

--- src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/03-10-01-the-basics/03-10-01-08/doc-03-10-01-08.component.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/03-10-01-the-basics/03-10-01-08/doc-03-10-01-08.component.ts
@@ -74,21 +74,21 @@ export class Doc03100108Component implements OnInit {
       }
     }
 
+    /*
+     * ActivatedRoute.parent 属性, 表示当前路由的上级路由
+     */
     if (this.activatedRoute.parent && this.activatedRoute.parent.routeConfig) {
-      /*
-       * ActivatedRoute.parent 属性, 表示当前路由的上级路由
-       */
       console.assert(this.activatedRoute.parent.routeConfig.path === '03/10');
     }
-
-    /*
-     * ActivatedRoute.children 属性, 表示当前路由的下级路由列表
-     */
-    console.assert(this.activatedRoute.children.length === 0);
 
     /*
      * ActivatedRoute.firstChild 属性, 表示当前路由的下级路由列表中第一个路由
      */
     console.assert(this.activatedRoute.firstChild === null);
+
+    /*
+     * ActivatedRoute.children 属性, 表示当前路由的下级路由列表
+     */
+    console.assert(this.activatedRoute.children.length === 0);
   }
 }
--- src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/doc-03-10.module.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/doc-03-10.module.ts
@@ -4,12 +4,12 @@ import { Doc0310RoutingModule } from './doc-03-10-routing.module';
 import { Doc0310Component } from './doc-03-10.component';
 import { Doc031001NotFoundComponent } from './03-10-01-the-basics/doc-03-10-01-not-found.component';
 import { Doc031001Component } from './03-10-01-the-basics/doc-03-10-01.component';
-import { RoutingNavigationBasicRouterEventsComponent } from './03-10-01-the-basics/router-events/routing-navigation-basic-router-events.component';
 import { Doc03100101Component } from './03-10-01-the-basics/03-10-01-01/doc-03-10-01-01.component';
 import { Doc03100104Component } from './03-10-01-the-basics/03-10-01-04/doc-03-10-01-04.component';
 import { Doc03100105Component } from './03-10-01-the-basics/03-10-01-05/doc-03-10-01-05.component';
 import { Doc03100106Component } from './03-10-01-the-basics/03-10-01-06/doc-03-10-01-06.component';
 import { Doc03100108Component } from './03-10-01-the-basics/03-10-01-08/doc-03-10-01-08.component';
+import { Doc03100109Component } from './03-10-01-the-basics/03-10-01-09/doc-03-10-01-09.component';
 
 @NgModule({
   declarations: [
@@ -20,8 +20,8 @@ import { Doc03100108Component } from './03-10-01-the-basics/03-10-01-08/doc-03-1
     Doc03100105Component,
     Doc03100106Component,
     Doc03100108Component,
+    Doc03100109Component,
     Doc031001NotFoundComponent,
-    RoutingNavigationBasicRouterEventsComponent,
   ],
   imports: [SharedModule, Doc0310RoutingModule],
 })
