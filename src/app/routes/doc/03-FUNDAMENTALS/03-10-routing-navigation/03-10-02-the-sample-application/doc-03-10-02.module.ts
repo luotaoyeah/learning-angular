14dad7be26bea4ad928b3a0c34024adcd4d63c95
luotao <luotaoyeah@gmail.com>
2019-06-18 10:59:34 +0800

    refactor(doc.routing.sample): Router.config

--- src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/03-10-02-the-sample-application/doc-03-10-02.module.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-10-routing-navigation/03-10-02-the-sample-application/doc-03-10-02.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { Router } from '@angular/router';
 import { SharedModule } from '@app/shared';
 import { Doc031002Component } from './doc-03-10-02.component';
 import { ComposeMessageComponent } from './compose-message/compose-message.component';
@@ -8,4 +9,14 @@ import { Doc031002RoutingModule } from './doc-03-10-02-routing.module';
   declarations: [Doc031002Component, ComposeMessageComponent],
   imports: [SharedModule, Doc031002RoutingModule],
 })
-export class Doc031002Module {}
+export class Doc031002Module {
+  constructor(router: Router) {
+    /*
+     * 通过 Router.config 属性, 可以获取应用整个的路由配置信息,
+     * 包含了所有的 feature module 中的路由配置,
+     * 但是没有包含尚未加载的 asynchronous feature module 的
+     */
+    // tslint:disable-next-line:no-any
+    (window as any).__ROUTE_CONFIG__ = router.config;
+  }
+}
