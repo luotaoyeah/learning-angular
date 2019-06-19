941afbaba6d1a36e9ceccaac1da1699aa99ce9f5
luotao <luotaoyeah@gmail.com>
2019-06-19 18:06:37 +0800

    refactor(doc.testing): setup

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-01-setup/doc-05-04-01.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-01-setup/doc-05-04-01.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#setup
+ */
 @Component({
   selector: 'app-doc-05-04-01',
   templateUrl: './doc-05-04-01.component.html',
 })
-export class Doc050401Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc050401Component {}
