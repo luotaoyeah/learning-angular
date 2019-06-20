8fe7fa824bdb983d070b3975fe3861e2388bb704
luotao <luotaoyeah@gmail.com>
2019-06-20 08:48:10 +0800

    refactor(doc.testing): service: TestBed

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-02-service-tests/services/doc-05-04-02.03.service.spec.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-02-service-tests/services/doc-05-04-02.03.service.spec.ts
@@ -1,3 +1,7 @@
+/*
+ * https://angular.io/guide/testing#testing-services-with-the-testbed
+ */
+
 import { Doc05040203Service } from './doc-05-04-02.03.service';
 import { TestBed } from '@angular/core/testing';
 import { Doc05040201Service } from './doc-05-04-02.01.service';
@@ -5,13 +9,13 @@ import SpyObj = jasmine.SpyObj;
 
 describe('Doc05040203Service', () => {
   let service: Doc05040203Service;
-  let spyObj: SpyObj<Doc05040201Service>;
+  let doc05040201ServiceSpyObj: SpyObj<Doc05040201Service>;
 
   beforeEach(() => {
     /*
-     * TestBed 是 angular test uitls 中一个重要的工具类，
-     * 通过 TestBedStatic.configureTestingModule() 方法可以创建一个用于测试的 test module，
-     * 类似一个 NgModule，通过这个 test module 可以实现 service 的依赖注入
+     * TestBed 是 angular testing utilities 中一个重要的工具类,
+     * 通过 TestBed.configureTestingModule() 方法可以创建一个专门用于测试的 test module,
+     * 类似一个 NgModule, 通过这个 test module 可以实现 service 的依赖注入
      */
     TestBed.configureTestingModule({
       providers: [
@@ -19,7 +23,7 @@ describe('Doc05040203Service', () => {
         {
           provide: Doc05040201Service,
           /*
-           * test module 的 providers 中，也可以使用 SpyObj 来设置某个 DI 的 provider 对象
+           * 可以使用一个 SpyObj 对象, 来作为某个依赖的注入值
            */
           useValue: jasmine.createSpyObj<Doc05040201Service>(
             'Doc05040201Service',
@@ -29,25 +33,26 @@ describe('Doc05040203Service', () => {
       ],
     });
 
-    spyObj = TestBed.get(Doc05040201Service);
+    /*
+     * 通过 TestBed.get() 方法获取注入器中的某个 service 实例
+     */
+    doc05040201ServiceSpyObj = TestBed.get(Doc05040201Service);
+    service = TestBed.get(Doc05040203Service);
   });
 
-  it('should return a value', () => {
-    spyObj.getValue.and.returnValue('foo');
+  it('should return a sync value by #getValue()', () => {
+    doc05040201ServiceSpyObj.getValue.and.returnValue('FOO');
 
-    /*
-     * 通过 TestBedStatic.get() 方法获取某个 service 实例
-     */
-    service = TestBed.get(Doc05040203Service);
-    expect(service.syncGet()).toBe('foo');
+    expect(service.getValue()).toBe('FOO');
   });
 
   it('should return a value from a spy obj', () => {
-    spyObj.getValue.and.returnValue('foo');
+    doc05040201ServiceSpyObj.getValue.and.returnValue('BAR');
 
-    service = TestBed.get(Doc05040203Service);
-    expect(service.syncGet()).toBe('foo');
-    expect(spyObj.getValue.calls.count()).toBe(1);
-    expect(spyObj.getValue.calls.mostRecent().returnValue).toBe('foo');
+    expect(service.getValue()).toBe('BAR');
+    expect(doc05040201ServiceSpyObj.getValue).toHaveBeenCalledTimes(1);
+    expect(
+      doc05040201ServiceSpyObj.getValue.calls.mostRecent().returnValue,
+    ).toBe('BAR');
   });
 });
--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-02-service-tests/services/doc-05-04-02.03.service.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-02-service-tests/services/doc-05-04-02.03.service.ts
@@ -5,9 +5,12 @@ import { Doc05040201Service } from './doc-05-04-02.01.service';
   providedIn: 'root',
 })
 export class Doc05040203Service {
-  constructor(private doc050702Service: Doc05040201Service) {}
+  constructor(private doc05040201Service: Doc05040201Service) {}
 
-  public syncGet(): string {
-    return this.doc050702Service.getValue();
+  /**
+   *
+   */
+  public getValue(): string {
+    return this.doc05040201Service.getValue();
   }
 }
