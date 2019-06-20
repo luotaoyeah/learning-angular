17e9d661c1e9ea13d14316d9132103eac2fdde51
luotao <luotaoyeah@gmail.com>
2019-06-20 09:35:55 +0800

    refactor(doc.testing): component: test component class

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-03-component-test-basics/05-04-03-01-component-class-testing/05-04-03-01-01/doc-05-04-03-01-01.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-03-component-test-basics/05-04-03-01-component-class-testing/05-04-03-01-01/doc-05-04-03-01-01.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#component-class-testing
+ */
 @Component({
   selector: 'app-doc-05-04-03-01-01',
   templateUrl: './doc-05-04-03-01-01.component.html',
 })
-export class Doc0504030101Component implements OnInit {
+export class Doc0504030101Component {
   public isOn: boolean = false;
 
   get message(): string {
-    return `the light is ${this.isOn ? 'ON' : 'OFF'}`;
+    return `${this.isOn ? 'ON' : 'OFF'}`;
   }
 
-  public ngOnInit() {}
-
-  public click(): void {
+  public handleClick(): void {
     this.isOn = !this.isOn;
   }
 }
--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-03-component-test-basics/doc-05-04-03.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-03-component-test-basics/doc-05-04-03.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#component-test-basics
+ */
 @Component({
   selector: 'app-doc-05-04-03',
   templateUrl: './doc-05-04-03.component.html',
 })
-export class Doc050403Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc050403Component {}
