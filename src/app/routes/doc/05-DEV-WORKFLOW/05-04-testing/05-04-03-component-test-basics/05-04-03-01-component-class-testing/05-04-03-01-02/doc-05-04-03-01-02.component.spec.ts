78f5fd073bc8b1fa65120375661df8aac214fefe
luotao <luotaoyeah@gmail.com>
2019-06-20 09:42:55 +0800

    refactor(doc.testing): component: test input property & output property

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-03-component-test-basics/05-04-03-01-component-class-testing/05-04-03-01-02/doc-05-04-03-01-02.component.spec.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-03-component-test-basics/05-04-03-01-component-class-testing/05-04-03-01-02/doc-05-04-03-01-02.component.spec.ts
@@ -3,15 +3,18 @@ import { Doc0504030102Component } from './doc-05-04-03-01-02.component';
 describe('Doc0504030102Component', () => {
   let component: Doc0504030102Component;
 
-  it('should raise myOutput event when #click()', () => {
+  /*
+   * 直接在 component class 中, 对 input 和 output 进行测试
+   */
+  it('should emit "myOutput" event when execute #handleClick()', () => {
     component = new Doc0504030102Component();
 
-    component.myInput = 'foobar';
+    component.myInput = 'foo';
 
     component.myOutput.subscribe((value: string) => {
-      expect(value).toBe('FOOBAR');
+      expect(value).toBe('FOO');
     });
 
-    component.click();
+    component.handleClick();
   });
 });
