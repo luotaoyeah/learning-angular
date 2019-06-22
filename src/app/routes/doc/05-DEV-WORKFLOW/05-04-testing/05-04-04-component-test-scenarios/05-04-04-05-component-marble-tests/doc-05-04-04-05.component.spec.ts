aa59037702698df2a9b71ced607bb02851ad8460
luotao <luotaoyeah@gmail.com>
2019-06-22 21:25:17 +0800

    refactor(doc.testing.component): marble error testing

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-05-component-marble-tests/doc-05-04-04-05.component.spec.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-05-component-marble-tests/doc-05-04-04-05.component.spec.ts
@@ -1,21 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  async,
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { SharedModule } from '@app/shared';
 import { Doc05040405Component } from './doc-05-04-04-05.component';
 import { Doc05040405Service } from './services/doc-05-04-04-05.service';
 import { getTestScheduler } from 'jasmine-marbles';
 import { RunHelpers } from 'rxjs/internal/testing/TestScheduler';
 import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('Doc05040405Component', () => {
   let fixture: ComponentFixture<Doc05040405Component>;
   let component: Doc05040405Component;
   let MockDoc05040405Service: jasmine.SpyObj<Doc05040405Service>;
   let buttonEl: HTMLButtonElement | null;
+  let spanEl: HTMLSpanElement | null;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Doc05040405Component],
-      imports: [SharedModule],
+      imports: [SharedModule, NoopAnimationsModule],
       providers: [
         {
           provide: Doc05040405Service,
@@ -34,12 +42,16 @@ describe('Doc05040405Component', () => {
     fixture = TestBed.createComponent(Doc05040405Component);
     component = fixture.componentInstance;
     buttonEl = (fixture.nativeElement as HTMLElement).querySelector('.num');
+    spanEl = (fixture.nativeElement as HTMLElement).querySelector('.ant-alert');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  /*
+   * https://angular.io/guide/testing#component-marble-tests
+   */
   it('should display num', () => {
     getTestScheduler().run(({ cold, flush }: RunHelpers) => {
       const observable01: ColdObservable<number> = cold('---x', {
@@ -59,4 +71,29 @@ describe('Doc05040405Component', () => {
       }
     });
   });
+
+  /*
+   * https://angular.io/guide/testing#marble-error-testing
+   */
+  it('should display the error message', fakeAsync(() => {
+    getTestScheduler().run(({ cold, flush }: RunHelpers) => {
+      const observable01$ = cold('---#|', undefined, new Error('ERR01'));
+
+      MockDoc05040405Service.getNextNum.and.returnValue(observable01$);
+
+      fixture.detectChanges();
+
+      flush();
+      tick();
+
+      fixture.detectChanges();
+
+      if (buttonEl) {
+        expect(buttonEl.textContent).toEqual('0');
+      }
+      if (spanEl) {
+        expect(spanEl.textContent).toEqual('ERR01');
+      }
+    });
+  }));
 });
