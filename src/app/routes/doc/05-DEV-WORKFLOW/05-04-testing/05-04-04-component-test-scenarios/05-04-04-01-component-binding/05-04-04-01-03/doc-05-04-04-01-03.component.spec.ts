991b1314b1b2b3c02007c1fdc0292831f11242e5
luotao <luotaoyeah@gmail.com>
2019-06-22 14:47:14 +0800

    refactor(doc.testing.component): dispatchEvent()

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-01-component-binding/05-04-04-01-03/doc-05-04-04-01-03.component.spec.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-01-component-binding/05-04-04-01-03/doc-05-04-04-01-03.component.spec.ts
@@ -1,56 +1,63 @@
-import { Doc0504040103Component } from './doc-05-04-04-01-03.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { NzButtonModule } from 'ng-zorro-antd';
+import { SharedModule } from '@app/shared';
+import { Doc0504040103Component } from './doc-05-04-04-01-03.component';
 
 describe('Doc0504040103Component', () => {
   let fixture: ComponentFixture<Doc0504040103Component>;
-  // @ts-ignore: TS6133
   let component: Doc0504040103Component;
+
+  let inputEl: HTMLInputElement | null;
+  let spanEl: HTMLSpanElement | null;
+
+  beforeAll(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 10;
+  });
+
+  afterAll(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 5;
+  });
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Doc0504040103Component],
-      imports: [FormsModule, NzButtonModule],
+      imports: [SharedModule],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(Doc0504040103Component);
     component = fixture.componentInstance;
+    const nativeElement = fixture.nativeElement as HTMLElement;
+    inputEl = nativeElement.querySelector<HTMLInputElement>('input');
+    spanEl = nativeElement.querySelector<HTMLSpanElement>('span');
   });
 
-  it('should convert title to uppercase', () => {
-    const nativeElement = fixture.nativeElement as HTMLElement;
-    const inputEl = nativeElement.querySelector<HTMLInputElement>('input');
-    const spanEl = nativeElement.querySelector<HTMLSpanElement>('span');
+  it('should be empty for #title at the beginning', () => {
+    expect(component.title).toEqual('');
+  });
+
+  it('should update #title after dispatch an `input` event', () => {
+    fixture.detectChanges();
 
     if (inputEl && spanEl) {
       /*
-       * 如何模拟用户输入？
+       * 如何模拟用户输入?
        *     1. 直接修改元素的 value 属性
        */
       inputEl.value = 'foo';
+      expect(component.title).toEqual('');
 
       /*
-       * 2. 触发一个 input 事件
+       * 2. 通过 dispatchEvent() 方法派发一个 input 事件
        */
-      inputEl.dispatchEvent(
-        new Event('input', {
-          bubbles: false,
-          cancelable: false,
-        }),
-      );
+      inputEl.dispatchEvent(new Event('input'));
+      expect(component.title).toEqual('foo');
 
       /*
        * 3. 触发一个 change detection
        */
       fixture.detectChanges();
-
-      /*
-       * FIXME 下面的测试失败
-       */
-      // expect(spanEl.textContent).toEqual('FOO');
+      expect(spanEl.textContent).toEqual('FOO');
     }
   });
 });
--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-01-component-binding/05-04-04-01-03/doc-05-04-04-01-03.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-01-component-binding/05-04-04-01-03/doc-05-04-04-01-03.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#change-an-input-value-with-dispatchevent
+ */
 @Component({
   selector: 'app-doc-05-04-04-01-03',
   templateUrl: './doc-05-04-04-01-03.component.html',
 })
-export class Doc0504040103Component implements OnInit {
+export class Doc0504040103Component {
   public title: string = '';
-
-  public ngOnInit() {}
 }
