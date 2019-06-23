7e8ec0d9659fdcfbd7c46c2fd7c347cab591d911
luotao <luotaoyeah@gmail.com>
2019-06-23 14:23:15 +0800

    refactor(doc.testing.component): stubbing unneeded components

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/05-04-04-10-01/doc-05-04-04-10-01.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/05-04-04-10-01/doc-05-04-04-10-01.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#stubbing-unneeded-components
+ */
 @Component({
   selector: 'app-doc-05-04-04-10-01',
   templateUrl: './doc-05-04-04-10-01.component.html',
 })
-export class Doc0504041001Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc0504041001Component {}
--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/doc-05-04-04-10.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/doc-05-04-04-10.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#nested-component-tests
+ */
 @Component({
   selector: 'app-doc-05-04-04-10',
   templateUrl: './doc-05-04-04-10.component.html',
 })
-export class Doc05040410Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc05040410Component {}
