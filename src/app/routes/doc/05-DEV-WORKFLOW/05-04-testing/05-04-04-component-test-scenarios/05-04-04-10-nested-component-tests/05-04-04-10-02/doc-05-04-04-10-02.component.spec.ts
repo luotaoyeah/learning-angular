3fd322f222eb189363f3e68f3ca2c79fd7339b58
luotao <luotaoyeah@gmail.com>
2019-06-23 14:29:00 +0800

    refactor(doc.testing.component): NO_ERRORS_SCHEMA

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/05-04-04-10-02/doc-05-04-04-10-02.component.spec.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/05-04-04-10-02/doc-05-04-04-10-02.component.spec.ts
@@ -10,10 +10,12 @@ describe('Doc0504041002Component', () => {
     TestBed.configureTestingModule({
       declarations: [Doc0504041002Component],
       /*
-       * 在测试 parent component 时，对于不关心的 child component，
-       * 一种方式是使用 stub component，
-       * 一种方式是设置 schemas 为 NO_ERRORS_SCHEMA
-       *     表示，所有 unknown element 和 unknown attribute 都会被忽略，
+       * 在测试 parent component 时, 对于我们不关心的 child component,
+       * 一种方式是使用 stub component,
+       * 一种方式是设置 schemas 为 NO_ERRORS_SCHEMA,
+       *     表示, 所有 unknown element 和 unknown attribute 都会被忽略
+       *
+       * 通常, 两种方式需要结合使用
        */
       schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/05-04-04-10-02/doc-05-04-04-10-02.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-10-nested-component-tests/05-04-04-10-02/doc-05-04-04-10-02.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#no_errors_schema
+ */
 @Component({
   selector: 'app-doc-05-04-04-10-02',
   templateUrl: './doc-05-04-04-10-02.component.html',
 })
-export class Doc0504041002Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc0504041002Component {}
