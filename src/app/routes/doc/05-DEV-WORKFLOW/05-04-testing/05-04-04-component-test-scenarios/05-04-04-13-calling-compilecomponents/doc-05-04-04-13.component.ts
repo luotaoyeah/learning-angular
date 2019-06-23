ffb6fa93d848423998c39fcdbde79744bd870c16
luotao <luotaoyeah@gmail.com>
2019-06-23 14:35:24 +0800

    refactor(doc.testing.component): TestBed.compileComponents

--- src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-13-calling-compilecomponents/doc-05-04-04-13.component.ts
+++ src/app/routes/doc/05-DEV-WORKFLOW/05-04-testing/05-04-04-component-test-scenarios/05-04-04-13-calling-compilecomponents/doc-05-04-04-13.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/testing#calling-compilecomponents
+ */
 @Component({
   selector: 'app-doc-05-04-04-13',
   templateUrl: './doc-05-04-04-13.component.html',
 })
-export class Doc05040413Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc05040413Component {}
