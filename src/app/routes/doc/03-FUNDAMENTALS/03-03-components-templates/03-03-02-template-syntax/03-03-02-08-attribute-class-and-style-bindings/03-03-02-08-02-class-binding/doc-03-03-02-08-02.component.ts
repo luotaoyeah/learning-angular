4de11e3c43eb6aa957197318e98954c570a54e74
luotao <luotaoyeah@gmail.com>
2019-06-26 17:40:51 +0800

    refactor(doc.fundamental.template): class binding

--- src/app/routes/doc/03-FUNDAMENTALS/03-03-components-templates/03-03-02-template-syntax/03-03-02-08-attribute-class-and-style-bindings/03-03-02-08-02-class-binding/doc-03-03-02-08-02.component.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-03-components-templates/03-03-02-template-syntax/03-03-02-08-attribute-class-and-style-bindings/03-03-02-08-02-class-binding/doc-03-03-02-08-02.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/template-syntax#class-binding
+ */
 @Component({
   selector: 'app-doc-03-03-02-08-02',
   templateUrl: './doc-03-03-02-08-02.component.html',
 })
-export class Doc0303020802Component implements OnInit {
-  public isFoo = false;
-
-  public ngOnInit() {}
+export class Doc0303020802Component {
+  public isFoo: boolean = false;
 }
