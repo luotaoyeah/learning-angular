ff2b79656bc19b95ee0144a28b6f8bc052c8bc23
luotao <luotaoyeah@gmail.com>
2019-06-26 21:27:43 +0800

    feat(a-gentle-introduction-into-change-detection-in-angular): component view and bindings

---
+++ src/app/routes/blog/a-gentle-introduction-into-change-detection-in-angular/02/x-02.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+/*
+ * https://blog.angularindepth.com/a-gentle-introduction-into-change-detection-in-angular-33f9ffff6f10#e7a1
+ */
+@Component({
+  selector: 'app-x-02',
+  templateUrl: './x-02.component.html',
+})
+export class X02Component {}
--- src/app/routes/blog/a-gentle-introduction-into-change-detection-in-angular/a-gentle-introduction-into-change-detection-in-angular.module.ts
+++ src/app/routes/blog/a-gentle-introduction-into-change-detection-in-angular/a-gentle-introduction-into-change-detection-in-angular.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
+import { SharedModule } from '@app/shared';
 import { AGentleIntroductionIntoChangeDetectionInAngularRoutingModule } from './a-gentle-introduction-into-change-detection-in-angular-routing.module';
 import { AGentleIntroductionIntoChangeDetectionInAngularComponent } from './a-gentle-introduction-into-change-detection-in-angular.component';
 import { X01Component } from './01/x-01.component';
-import { SharedModule } from '@app/shared';
+import { X02Component } from './02/x-02.component';
 
 @NgModule({
   declarations: [
     AGentleIntroductionIntoChangeDetectionInAngularComponent,
     X01Component,
+    X02Component,
   ],
   imports: [
     SharedModule,
