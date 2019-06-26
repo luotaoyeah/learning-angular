98fca3f4a47cc488c80f004e537d5a73c9adffdc
luotao <luotaoyeah@gmail.com>
2019-06-26 17:55:44 +0800

    refactor(doc.fundamental.template): style binding

--- src/app/routes/doc/03-FUNDAMENTALS/03-03-components-templates/03-03-02-template-syntax/03-03-02-08-attribute-class-and-style-bindings/03-03-02-08-03-style-binding/doc-03-03-02-08-03.component.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-03-components-templates/03-03-02-template-syntax/03-03-02-08-attribute-class-and-style-bindings/03-03-02-08-03-style-binding/doc-03-03-02-08-03.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/template-syntax#style-binding
+ */
 @Component({
   selector: 'app-doc-03-03-02-08-03',
   templateUrl: './doc-03-03-02-08-03.component.html',
 })
-export class Doc0303020803Component implements OnInit {
-  public ngOnInit() {}
-}
+export class Doc0303020803Component {}
