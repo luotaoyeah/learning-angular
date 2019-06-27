921bafd5391a5ff9d0d89b436f302c96540e2785
luotao <luotaoyeah@gmail.com>
2019-06-27 10:03:45 +0800

    feat(a-gentle-introduction-into-change-detection-in-angular): order of operations

--- src/app/routes/blog/a-gentle-introduction-into-change-detection-in-angular/a-gentle-introduction-into-change-detection-in-angular.module.ts
+++ src/app/routes/blog/a-gentle-introduction-into-change-detection-in-angular/a-gentle-introduction-into-change-detection-in-angular.module.ts
@@ -6,6 +6,8 @@ import { X01Component } from './01/x-01.component';
 import { X02Component } from './02/x-02.component';
 import { X03Component } from './03/x-03.component';
 import { X04Component } from './04/x-04.component';
+import { X05Component } from './05/x-05.component';
+import { X0501Component } from './05/05-01/x-05-01.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { X04Component } from './04/x-04.component';
     X02Component,
     X03Component,
     X04Component,
+    X05Component,
+    X0501Component,
   ],
   imports: [
     SharedModule,
