7477566257ad6e167415d2fe064254e2496661ba
luotao <luotaoyeah@gmail.com>
2019-06-29 20:55:11 +0800

    feat(blog.here-is-what-you-need-to-know-about-dynamic-components): ngOnChanges()

---
+++ src/app/routes/blog/here-is-what-you-need-to-know-about-dynamic-components/06/x-06.component.ts
@@ -0,0 +1,45 @@
+import {
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
+import { X0601Component } from './01/x-06-01.component';
+
+/*
+ * https://blog.angularindepth.com/here-is-what-you-need-to-know-about-dynamic-components-in-angular-ac1e96167f9e#4591
+ */
+@Component({
+  selector: 'app-x-06',
+  templateUrl: './x-06.component.html',
+})
+export class X06Component {
+  @ViewChild('vc', {
+    static: false,
+    read: ViewContainerRef,
+  })
+  public vc!: ViewContainerRef;
+
+  public x0601ComponentComponentRef: ComponentRef<X0601Component> | null = null;
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {}
+
+  public createComponent() {
+    this.vc.clear();
+    this.x0601ComponentComponentRef = this.vc.createComponent(
+      this.componentFactoryResolver.resolveComponentFactory(X0601Component),
+    );
+    this.x0601ComponentComponentRef.instance.foo = Date.now().toString();
+  }
+
+  /*
+   * 动态创建的组件, 它的 input property 不是在模板中通过 property binding 绑定的,
+   * 而是通过直接修改组件属性的方式来更新, 因此动态组件不会触发 ngOnChanges() 方法
+   */
+  public changeInput() {
+    if (this.x0601ComponentComponentRef) {
+      this.x0601ComponentComponentRef.instance.foo = Date.now().toString();
+    }
+  }
+}
--- src/app/routes/blog/here-is-what-you-need-to-know-about-dynamic-components/here-is-what-you-need-to-know-about-dynamic-components.module.ts
+++ src/app/routes/blog/here-is-what-you-need-to-know-about-dynamic-components/here-is-what-you-need-to-know-about-dynamic-components.module.ts
@@ -12,6 +12,8 @@ import { X04Component } from './04/x-04.component';
 import { X05Component } from './05/x-05.component';
 import { X0501Component } from './05/01/x-05-01.component';
 import { X050101Component } from './05/01/x-05-01-01.component';
+import { X06Component } from './06/x-06.component';
+import { X0601Component } from './06/01/x-06-01.component';
 
 @NgModule({
   declarations: [
@@ -25,11 +27,13 @@ import { X050101Component } from './05/01/x-05-01-01.component';
     X05Component,
     X0501Component,
     X050101Component,
+    X06Component,
+    X0601Component,
   ],
   imports: [
     HereIsWhatYouNeedToKnowAboutDynamicComponentsRoutingModule,
     SharedModule,
   ],
-  entryComponents: [NzAlertComponent, X050101Component],
+  entryComponents: [NzAlertComponent, X050101Component, X0601Component],
 })
 export class HereIsWhatYouNeedToKnowAboutDynamicComponentsModule {}
