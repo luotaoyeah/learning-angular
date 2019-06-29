82d84b5b66b2a2bfbb5b5fdf0623e9bb928da037
luotao <luotaoyeah@gmail.com>
2019-06-29 18:47:32 +0800

    feat(blog.here-is-what-you-need-to-know-about-dynamic-components): create components on the fly

--- src/app/routes/blog/here-is-what-you-need-to-know-about-dynamic-components/here-is-what-you-need-to-know-about-dynamic-components.module.ts
+++ src/app/routes/blog/here-is-what-you-need-to-know-about-dynamic-components/here-is-what-you-need-to-know-about-dynamic-components.module.ts
@@ -8,6 +8,7 @@ import { X02Component } from './02/x-02.component';
 import { X03Component } from './03/x-03.component';
 import { X0301Component } from './03/01/x-03-01.component';
 import { X0302Component } from './03/02/x-03-02.component';
+import { X04Component } from './04/x-04.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { X0302Component } from './03/02/x-03-02.component';
     X03Component,
     X0301Component,
     X0302Component,
+    X04Component,
   ],
   imports: [
     HereIsWhatYouNeedToKnowAboutDynamicComponentsRoutingModule,
