f530b5b105a24ab17429563cdbb777b09f57bae1
luotao <luotaoyeah@gmail.com>
2019-07-02 21:28:57 +0800

    feat(pont): multiple origins

--- _mock/api/swagger/swagger.controller.ts
+++ _mock/api/swagger/swagger.controller.ts
@@ -1,13 +1,13 @@
-/**
- * @backend Kengic.SystemSetup.API.Controllers.UserController
- */
-
 import { Application } from 'express';
 import { Request, Response } from 'express-serve-static-core';
 
 function init(app: Application) {
   app.get('/SortingPd/swagger.json', (req: Request, res: Response) => {
     res.jsonp(require('./sorting-pd.swagger.json'));
+  });
+
+  app.get('/SortingDr/swagger.json', (req: Request, res: Response) => {
+    res.jsonp(require('./sorting-dr.swagger.json'));
   });
 }
 
