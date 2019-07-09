f293aea6678695c27875e541892d9fee90ffd2dd
luotao <luotaoyeah@gmail.com>
2019-07-09 21:08:00 +0800

    chore(blog.do-you-still-think-that-ngzone-is-required-for-change-detection-in-angular): common pitfalls

--- src/app/routes/blog/do-you-still-think-that-ngzone-is-required-for-change-detection-in-angular/do-you-still-think-that-ngzone-is-required-for-change-detection-in-angular.module.ts
+++ src/app/routes/blog/do-you-still-think-that-ngzone-is-required-for-change-detection-in-angular/do-you-still-think-that-ngzone-is-required-for-change-detection-in-angular.module.ts
@@ -4,12 +4,14 @@ import { DoYouStillThinkThatNgzoneIsRequiredForChangeDetectionInAngularRoutingMo
 import { DoYouStillThinkThatNgzoneIsRequiredForChangeDetectionInAngularComponent } from './do-you-still-think-that-ngzone-is-required-for-change-detection-in-angular.component';
 import { X01Component } from './01/x-01.component';
 import { X02Component } from './02/x-02.component';
+import { X03Component } from './03/x-03.component';
 
 @NgModule({
   declarations: [
     DoYouStillThinkThatNgzoneIsRequiredForChangeDetectionInAngularComponent,
     X01Component,
     X02Component,
+    X03Component,
   ],
   imports: [
     DoYouStillThinkThatNgzoneIsRequiredForChangeDetectionInAngularRoutingModule,
