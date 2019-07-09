1e93e801ceb056fe4d875720920309dec5cc6cb4
luotao <luotaoyeah@gmail.com>
2019-07-09 22:00:44 +0800

    chore(blog.everything-you-need-to-know-about-change-detection): ViewState

--- src/app/routes/blog/everything-you-need-to-know-about-change-detection/everything-you-need-to-know-about-change-detection.module.ts
+++ src/app/routes/blog/everything-you-need-to-know-about-change-detection/everything-you-need-to-know-about-change-detection.module.ts
@@ -3,11 +3,13 @@ import { SharedModule } from '@app/shared';
 import { EverythingYouNeedToKnowAboutChangeDetectionRoutingModule } from './everything-you-need-to-know-about-change-detection-routing.module';
 import { EverythingYouNeedToKnowAboutChangeDetectionComponent } from './everything-you-need-to-know-about-change-detection.component';
 import { X01Component } from './01/x-01.component';
+import { X02Component } from './02/x-02.component';
 
 @NgModule({
   declarations: [
     EverythingYouNeedToKnowAboutChangeDetectionComponent,
     X01Component,
+    X02Component,
   ],
   imports: [
     EverythingYouNeedToKnowAboutChangeDetectionRoutingModule,
