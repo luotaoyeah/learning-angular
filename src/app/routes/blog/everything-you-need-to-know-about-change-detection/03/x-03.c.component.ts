20670595a7cdccd30c6b37064e8afa718c33f238
luotao <luotaoyeah@gmail.com>
2019-07-10 19:32:29 +0800

    chore(blog.everything-you-need-to-know-about-change-detection): change detection operations

---
+++ src/app/routes/blog/everything-you-need-to-know-about-change-detection/03/x-03.c.component.ts
@@ -0,0 +1,34 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-x-03-c',
+  template: '',
+})
+export class X03CComponent
+  implements
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked {
+  public ngAfterContentInit(): void {
+    console.log('%c\t\tC: ngAfterContentInit()', 'color:green');
+  }
+
+  public ngAfterContentChecked(): void {
+    console.log('%c\t\tC: ngAfterContentChecked()', 'color:green');
+  }
+
+  public ngAfterViewInit(): void {
+    console.log('%c\t\tC: ngAfterViewInit()', 'color:green');
+  }
+
+  public ngAfterViewChecked(): void {
+    console.log('%c\t\tC: ngAfterViewChecked()', 'color:green');
+  }
+}
--- src/app/routes/blog/everything-you-need-to-know-about-change-detection/everything-you-need-to-know-about-change-detection.module.ts
+++ src/app/routes/blog/everything-you-need-to-know-about-change-detection/everything-you-need-to-know-about-change-detection.module.ts
@@ -4,12 +4,20 @@ import { EverythingYouNeedToKnowAboutChangeDetectionRoutingModule } from './ever
 import { EverythingYouNeedToKnowAboutChangeDetectionComponent } from './everything-you-need-to-know-about-change-detection.component';
 import { X01Component } from './01/x-01.component';
 import { X02Component } from './02/x-02.component';
+import { X03Component } from './03/x-03.component';
+import { X03CComponent } from './03/x-03.c.component';
+import { X03AComponent } from './03/x-03.a.component';
+import { X03BComponent } from './03/x-03.b.component';
 
 @NgModule({
   declarations: [
     EverythingYouNeedToKnowAboutChangeDetectionComponent,
     X01Component,
     X02Component,
+    X03Component,
+    X03AComponent,
+    X03BComponent,
+    X03CComponent,
   ],
   imports: [
     EverythingYouNeedToKnowAboutChangeDetectionRoutingModule,
