09d3e58efd37e88a7bdaaff983be78f2f509e8b8
luotao <luotaoyeah@gmail.com>
2019-07-11 21:43:46 +0800

    chore(blog.boosting-performance-of-angular-applications-with-manual-change-detection): NgZone.runOutsideAngular()

--- src/app/routes/blog/boosting-performance-of-angular-applications-with-manual-change-detection/boosting-performance-of-angular-applications-with-manual-change-detection.module.ts
+++ src/app/routes/blog/boosting-performance-of-angular-applications-with-manual-change-detection/boosting-performance-of-angular-applications-with-manual-change-detection.module.ts
@@ -3,11 +3,13 @@ import { SharedModule } from '@app/shared';
 import { BoostingPerformanceOfAngularApplicationsWithManualChangeDetectionRoutingModule } from './boosting-performance-of-angular-applications-with-manual-change-detection-routing.module';
 import { BoostingPerformanceOfAngularApplicationsWithManualChangeDetectionComponent } from './boosting-performance-of-angular-applications-with-manual-change-detection.component';
 import { X01Component } from './01/x-01.component';
+import { X02Component } from './02/x-02.component';
 
 @NgModule({
   declarations: [
     BoostingPerformanceOfAngularApplicationsWithManualChangeDetectionComponent,
     X01Component,
+    X02Component,
   ],
   imports: [
     BoostingPerformanceOfAngularApplicationsWithManualChangeDetectionRoutingModule,
