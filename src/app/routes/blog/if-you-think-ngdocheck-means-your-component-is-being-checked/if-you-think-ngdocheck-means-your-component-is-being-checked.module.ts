1b251bef0721d08e8f66158af7cdc3d2e815284d
luotao <luotaoyeah@gmail.com>
2019-07-12 09:37:17 +0800

    chore(blog.if-you-think-ngdocheck-means-your-component-is-being-checked): ngDoCheck()

--- src/app/routes/blog/if-you-think-ngdocheck-means-your-component-is-being-checked/if-you-think-ngdocheck-means-your-component-is-being-checked.module.ts
+++ src/app/routes/blog/if-you-think-ngdocheck-means-your-component-is-being-checked/if-you-think-ngdocheck-means-your-component-is-being-checked.module.ts
@@ -3,11 +3,13 @@ import { SharedModule } from '@app/shared';
 import { IfYouThinkNgdocheckMeansYourComponentIsBeingCheckedRoutingModule } from './if-you-think-ngdocheck-means-your-component-is-being-checked-routing.module';
 import { IfYouThinkNgdocheckMeansYourComponentIsBeingCheckedComponent } from './if-you-think-ngdocheck-means-your-component-is-being-checked.component';
 import { X01Component } from './01/x-01.component';
+import { X01AComponent } from './01/x-01.a.component';
 
 @NgModule({
   declarations: [
     IfYouThinkNgdocheckMeansYourComponentIsBeingCheckedComponent,
     X01Component,
+    X01AComponent,
   ],
   imports: [
     IfYouThinkNgdocheckMeansYourComponentIsBeingCheckedRoutingModule,
