d0dd639980ed8545e32aa680bfdaddb675311a64
luotao <luotaoyeah@gmail.com>
2019-07-15 17:09:28 +0800

    feat(guide.testing): components with RouterLink

--- src/app/routes/blog/if-you-think-ngdocheck-means-your-component-is-being-checked/02/x-02.b.component.ts
+++ src/app/routes/blog/if-you-think-ngdocheck-means-your-component-is-being-checked/02/x-02.b.component.ts
@@ -21,7 +21,7 @@ export class X02BComponent implements DoCheck {
 
   /*
    * 当组件使用了 OnPush 策略, 同时 input property 的内部属性发生了变更时, 如何才能触发 CD ? 这时候, ngDoCheck() 就派上了用场,
-   * 即使组件使用了 OnPush 策略, 但是当上级组件发成 CD 时, 该组件的 ngDoCheck() 依然会触发,
+   * 即使组件使用了 OnPush 策略, 但是当上级组件发生 CD 时, 该组件的 ngDoCheck() 依然会触发,
    * 我们可以在它里面, 对 input property 的属性进行比较, 从而判断是否需要执行 CD, 如果需要执行 CD, 则可以使用 ChangeDetectorRef.markForCheck() 进行标记,
    * 从而触发 CD
    */
