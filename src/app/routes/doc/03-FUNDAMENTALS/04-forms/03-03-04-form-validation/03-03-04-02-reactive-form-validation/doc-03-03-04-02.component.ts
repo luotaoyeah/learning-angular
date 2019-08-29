59c01cf49d9c3cfabb41b41bd4c8fff09159d830
luotao <luotaoyeah@gmail.com>
2019-08-29 14:21:08 +0800

    refactor(doc.guide.form-validation): reactive-form-validation

--- src/app/routes/doc/03-FUNDAMENTALS/04-forms/03-03-04-form-validation/03-03-04-02-reactive-form-validation/doc-03-03-04-02.component.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/04-forms/03-03-04-form-validation/03-03-04-02-reactive-form-validation/doc-03-03-04-02.component.ts
@@ -7,14 +7,21 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-doc-03-03-04-02',
   templateUrl: './doc-03-03-04-02.component.html',
+  styleUrls: ['./doc-03-03-04-02.component.less'],
 })
 export class Doc03030402Component {
   public formGroup: FormGroup = new FormGroup({
     name: new FormControl('', {
+      /* 通过 validators 选项来设置同步的验证函数 */
       validators: [Validators.required, Validators.minLength(4)],
+      /* 通过 asyncValidators 选项来设置异步的验证函数 */
+      asyncValidators: [],
     }),
   });
 
+  /*
+   * 为了方便访问某个 control 实例, 可以通过 getter 函数来访问该 control 实例
+   */
   get nameControl(): FormControl {
     return this.formGroup.get(['name']) as FormControl;
   }
