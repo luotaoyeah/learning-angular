4e1ca63057167db89e49f30972d9546bc1d74ca1
luotao <luotaoyeah@gmail.com>
2019-08-30 19:08:52 +0800

    refactor(doc.guide.component-interaction): parent-interacts-with-child-via-local-variable

--- src/app/routes/doc/03-FUNDAMENTALS/03-components-templates/05-component-interaction/05-parent-interacts-with-child-via-local-variable/doc-03-03-05-05.component.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-components-templates/05-component-interaction/05-parent-interacts-with-child-via-local-variable/doc-03-03-05-05.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+/*
+ * https://angular.io/guide/component-interaction#parent-interacts-with-child-via-local-variable
+ */
 @Component({
   selector: 'app-doc-03-03-05-05',
   templateUrl: './doc-03-03-05-05.component.html',
