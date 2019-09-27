bd8cd1907525acc169046938eaf6c2fc01c0efa9
luotao <luotaoyeah@gmail.com>
2019-09-27 18:43:50 +0800

    refactor(fundamental.components-templates.displaying-data): remove p-accordion

--- src/app/routes/doc/03-FUNDAMENTALS/03-components-templates/01-displaying-data/03-02-01-03/doc-03-02-01-03.component.ts
+++ src/app/routes/doc/03-FUNDAMENTALS/03-components-templates/01-displaying-data/03-02-01-03/doc-03-02-01-03.component.ts
@@ -3,13 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-doc-03-02-01-03',
   template: `
-    <p-accordionTab
-      header="Constructor or Variable Initialization ?"
-      [transitionOptions]="'0ms'"
-    >
-      <p>组件中的属性可以在定义属性时就进行初始化：{{ foo }}</p>
-      <p>也可以在构造函数（constructor）中进行赋值：{{ bar }}</p>
-    </p-accordionTab>
+    <p>组件中的属性可以在定义属性时就进行初始化：{{ foo }}</p>
+    <p>也可以在构造函数（constructor）中进行赋值：{{ bar }}</p>
   `,
 })
 export class Doc03020103Component implements OnInit {
