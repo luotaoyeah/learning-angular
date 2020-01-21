6bb97d81aaccb9472dd656e8ffb34d0feaa4f3d9
luotao <luotaoyeah@gmail.com>
2020-01-21 20:21:35 +0800

    fix(learning-angular-lib): export ngx-jquery-slider.component

--- projects/learning-angular-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of learning-angular-lib
- */
-
-export * from './lib/learning-angular-lib.service';
-export * from './lib/components/learning-angular-lib.component';
-export * from './lib/learning-angular-lib.module';
