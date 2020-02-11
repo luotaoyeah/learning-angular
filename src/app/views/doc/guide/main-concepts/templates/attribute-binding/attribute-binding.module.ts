42ddff195901755930a4f67f057bc92e289d8d05
luotao <luotaoyeah@gmail.com>
2020-02-11 20:33:28 +0800

    refactor(guide): attribute-binding#binding-to-the-style-attribute

--- src/app/views/doc/guide/main-concepts/templates/attribute-binding/attribute-binding.module.ts
+++ src/app/views/doc/guide/main-concepts/templates/attribute-binding/attribute-binding.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Doc0303020801Component } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/03-03-02-08-01-attribute-binding/doc-03-03-02-08-01.component';
-import { BindingToTheClassAttributeComponent } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/binding-to-the-class-attribute.component';
-import { Doc0303020803Component } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/03-03-02-08-03-style-binding/doc-03-03-02-08-03.component';
-import { AttributeBindingRoutingModule } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/attribute-binding.routing.module';
 import { AttributeBindingComponent } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/attribute-binding.component';
+import { AttributeBindingRoutingModule } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/attribute-binding.routing.module';
+import { BindingToTheClassAttributeComponent } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/binding-to-the-class-attribute.component';
+import { BindingToTheStyleAttributeComponent } from '@app/views/doc/guide/main-concepts/templates/attribute-binding/binding-to-the-style-attribute.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
 @NgModule({
   declarations: [
     AttributeBindingComponent,
     Doc0303020801Component,
     BindingToTheClassAttributeComponent,
-    Doc0303020803Component,
+    BindingToTheStyleAttributeComponent,
   ],
-  imports: [AttributeBindingRoutingModule, NzButtonModule, NzCollapseModule],
+  imports: [AttributeBindingRoutingModule, NzButtonModule],
 })
 export class AttributeBindingModule {}
---
+++ src/app/views/doc/guide/main-concepts/templates/attribute-binding/binding-to-the-style-attribute.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+// https://angular.io/guide/attribute-binding#binding-to-the-style-attribute
+@Component({
+  selector: 'app--binding-to-the-style-attribute',
+  templateUrl: './binding-to-the-style-attribute.component.html',
+})
+export class BindingToTheStyleAttributeComponent {}
