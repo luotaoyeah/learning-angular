ed4038f5e41df5ceacca9c4b9c56ab7b54a4c2d1
luotao <luotaoyeah@gmail.com>
2022-02-27 16:21:40 +0800

    feat(projects/content-projection): https://angular.io/guide/content-projection#multi-slot-content-projection

--- projects/content-projection/src/app/app.module.ts
+++ projects/content-projection/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CompAComponent } from './comp-a/comp-a.component';
+import { CompBComponent } from './comp-b/comp-b.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CompAComponent
+    CompAComponent,
+    CompBComponent
   ],
   imports: [
     BrowserModule,
---
+++ projects/content-projection/src/app/comp-b/comp-b.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-comp-b',
+    templateUrl: './comp-b.component.html',
+    styleUrls: ['./comp-b.component.less'],
+})
+export class CompBComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit(): void {}
+}
