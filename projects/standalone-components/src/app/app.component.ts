72ae75beb2bca8481c18f460341575593163c159
luotao <luotaoyeah@gmail.com>
2022-03-05 11:33:41 +0800

    feat(standalone-components): add test

--- projects/standalone-components/src/app/app.component.ts
+++ projects/standalone-components/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'standalone-components';
+    title = '@luotao/learning-angular.standalone-components';
 }
