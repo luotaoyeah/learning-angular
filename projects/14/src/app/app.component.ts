750c4f7230e0de892f9f82a13b3ec7a8718e79c9
luotao <luotaoyeah@gmail.com>
2022-05-17 21:24:09 +0800

    feat(14): useFactory

--- projects/14/src/app/app.component.ts
+++ projects/14/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { TOKEN_A, TOKEN_B, TOKEN_C } from './element-injector-hierarchy/a.const';
+import { Component, Inject } from '@angular/core';
+import { TOKEN_A, TOKEN_B, TOKEN_C, TOKEN_E } from './element-injector-hierarchy/a.const';
 import { LoggerService } from './services/logger.service';
 import { LoggerAService } from './services/logger-a.service';
+import { ILoggerService } from './services/i-logger.service';
 
 @Component({
     selector: 'app-root',
@@ -13,11 +14,26 @@ import { LoggerAService } from './services/logger-a.service';
         { provide: TOKEN_A, useValue: 'AppComponent' },
         { provide: TOKEN_B, useValue: 'AppComponent' },
         { provide: TOKEN_C, useValue: 'AppComponent' },
+        {
+            provide: TOKEN_E,
+            useFactory: (a: string, c: string) => {
+                console.log('TOKEN_E: a:', a);
+                console.log('TOKEN_E: c:', c);
+                return 'AppComponent';
+            },
+            deps: [TOKEN_A, TOKEN_C],
+        },
         { provide: LoggerService, useClass: LoggerAService },
     ],
 })
 export class AppComponent {
-    public constructor(private loggerService: LoggerService) {
+    public constructor(
+        @Inject(LoggerService)
+        private loggerService: ILoggerService,
+        @Inject(TOKEN_E)
+        private e: string,
+    ) {
         this.loggerService.log('AppComponent');
+        console.log('e:', e);
     }
 }
--- projects/14/src/app/element-injector-hierarchy/a.const.ts
+++ projects/14/src/app/element-injector-hierarchy/a.const.ts
@@ -4,5 +4,6 @@ const TOKEN_A = new InjectionToken<string>('A');
 const TOKEN_B = new InjectionToken<string>('B');
 const TOKEN_C = new InjectionToken<string>('C');
 const TOKEN_D = new InjectionToken<string>('D');
+const TOKEN_E = new InjectionToken<string>('E');
 
-export { TOKEN_A, TOKEN_B, TOKEN_C, TOKEN_D };
+export { TOKEN_A, TOKEN_B, TOKEN_C, TOKEN_D, TOKEN_E };
